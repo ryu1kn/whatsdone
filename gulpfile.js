f42f44b2be18f9c8845e329e674251109b538d6d
ryu1kn <ryuichi.inagaki.24@googlemail.com>
2015-06-28 20:03:19 +0900

    WD-11 #done Adopt LESS

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,39 @@ var browserify = require('browserify'),
     source = require('vinyl-source-stream'),
     gulp  = require('gulp');
 
+var less = require('gulp-less');
+
 var paths = {
         js: {
             inEntry: './src/client/scripts/app.js',
             inAll  : './src/client/scripts/**/*.js',
             outName: 'app.js',
             outPath: './public/scripts'
+        },
+        css: {
+            in: './src/stylesheets/**/*.less',
+            out: './public/css'
         }
     };
 
+gulp.task('compile', ['compilejs', 'compilecss']);
+
 gulp.task('watch', function() {
-    gulp.watch(paths.js.inAll, ['compile']);
+    gulp.watch(paths.js.inAll, ['compilejs']);
+    gulp.watch(paths.css.in, ['compilecss']);
 });
 
-gulp.task('compile', function () {
+gulp.task('compilejs', function () {
     return browserify(paths.js.inEntry)
         .bundle()
         .pipe(source(paths.js.outName))
         .pipe(gulp.dest(paths.js.outPath));
 });
 
-gulp.task('default', ['compile', 'watch']);
+gulp.task('compilecss', function () {
+  return gulp.src(paths.css.in)
+    .pipe(less())
+    .pipe(gulp.dest(paths.css.out));
+});
+
+gulp.task('default', ['compilejs', 'compilecss', 'watch']);
