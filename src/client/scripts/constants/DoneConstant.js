fddce42e4fd1f867128cc7352e6a053337e8d9b4
ryu1kn <ryuichi.inagaki.24@googlemail.com>
2015-10-11 20:17:19 +1100

    Fix that wrong constants were being commented in/out

--- src/client/scripts/constants/DoneConstant.js
+++ src/client/scripts/constants/DoneConstant.js
@@ -3,7 +3,6 @@ var keyMirror = require('keymirror');
 
 module.exports = keyMirror({
   DONE_CREATE: null,
-  // DONE_CREATE_COMPLETE: null,
-  DONE_DESTROY: null,
-  DONE_UPDATE: null
+  DONE_CREATE_COMPLETE: null,
+  DONE_DESTROY: null
 });
