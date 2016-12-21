937be65c5fc5cf56871ac931fcf1e0b4eb10b8ad
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-12-21 23:33:12 +1100

    Trying with AWS SDK for DynamoDB

--- src/main/java/whatsdone_cli/DoneRepo.java
+++ src/main/java/whatsdone_cli/DoneRepo.java
@@ -1,6 +1,53 @@
 package whatsdone_cli;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+
 public class DoneRepo {
+
+    // From AWS Sample Code
+    public void printTableContents() throws Exception {
+        AWSCredentials credentials = null;
+        try {
+            credentials = new ProfileCredentialsProvider().getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (~/.aws/credentials), and is in valid format.",
+                    e);
+        }
+        AmazonDynamoDBClient dynamoDB = new AmazonDynamoDBClient(credentials);
+        Region region = Region.getRegion(Regions.AP_SOUTHEAST_2);
+        dynamoDB.setRegion(region);
+
+        try {
+            String tableName = "ryuichi-test-db";
+            ScanRequest scanRequest = new ScanRequest(tableName);
+            ScanResult scanResult = dynamoDB.scan(scanRequest);
+            System.out.println("Result: " + scanResult);
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which means your request made it "
+                    + "to AWS, but was rejected with an error response for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with AWS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+    }
 
     String getAll() {
         return null;
--- src/main/java/whatsdone_cli/Main.java
+++ src/main/java/whatsdone_cli/Main.java
@@ -4,7 +4,13 @@ package whatsdone_cli;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("App started...");
+        DoneRepo repo = new DoneRepo();
+        try {
+            repo.printTableContents();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
