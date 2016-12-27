55a4cf52982be54e46f1dd541b8ccdb1b7401554
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-12-27 19:18:10 +1100

    Split server/client side code directory
    so that the scope of code is more visible

--- src/server/app.js
+++ src/server/app.js
@@ -6,13 +6,13 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var context = require('./src/service/context');
+var context = require('./service/context');
 
 var app = express();
 context.setEnv(process.env);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'src', 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // TODO: uncomment after placing a favicon in /public
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', '..', 'public')));
 app.use(session({
   secret: 'keyboard cat',
   saveUninitialized: false,   // don't create session until something stored
@@ -48,10 +48,10 @@ app.all('*', function (req, res, next) {
   }
 });
 
-app.use('/', require('./src/routes/index'));
-app.use('/signin', require('./src/routes/signin'));
-app.use('/signout', require('./src/routes/signout'));
-app.use('/dones.json', require('./src/routes/dones'));
+app.use('/', require('./routes/index'));
+app.use('/signin', require('./routes/signin'));
+app.use('/signout', require('./routes/signout'));
+app.use('/dones.json', require('./routes/dones'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
