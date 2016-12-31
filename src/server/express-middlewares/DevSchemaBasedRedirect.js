9f028206351a9e39132053674e30f0e0362e8fb5
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-12-31 20:15:51 +1100

    Put debug statement to investigate infinite redirection

--- src/server/express-middlewares/DevSchemaBasedRedirect.js
+++ src/server/express-middlewares/DevSchemaBasedRedirect.js
@@ -5,6 +5,7 @@
 class SchemaBasedRedirectMiddleware {
 
   handle(req, res, next) {
+    console.log('Connecting protocol @DevSchemaBasedRedirectMiddleware:', req.protocol);
     next();
   }
 
--- src/server/express-middlewares/SchemaBasedRedirect.js
+++ src/server/express-middlewares/SchemaBasedRedirect.js
@@ -7,6 +7,7 @@ const SECURE_PROTOCOL = 'https';
 class SchemaBasedRedirectMiddleware {
 
   handle(req, res, next) {
+    console.log('Connecting protocol @SchemaBasedRedirectMiddleware:', req.protocol);
     if (req.protocol === SECURE_PROTOCOL) next();
     else res.redirect(`${SECURE_PROTOCOL}://${req.hostname}${req.url}`);
   }
