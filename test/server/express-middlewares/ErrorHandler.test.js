36893db7938d232fa038cdf7b53a546b048069e0
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-01-28 22:30:40 +1100

    Added tests for ErrorHandler to confirm current implementation

--- test/server/express-middlewares/ErrorHandler.test.js
+++ test/server/express-middlewares/ErrorHandler.test.js
@@ -18,7 +18,38 @@ describe('Server ErrorHandler', () => {
       res.render('error', {message: ''});
       expect(logger.error.args[0][0]).to.have.string('Error: [NotFound]: NOT_FOUND');
     });
+  });
 
+  it('shows an error page with the information that access was denied', () => {
+    const logger = {error: sinon.spy()};
+    ServiceLocator.load({
+      createLogger: () => logger
+    });
+    const middleware = new ErrorHandler();
+
+    const req = {};
+    const err = new Error('[AccessDeined]: ACCESS_DENIED');
+    return promisifyExpressMiddleware(middleware, req, err).then(({res}) => {
+      expect(res.status).to.have.been.calledWith(403);
+      expect(res.render).to.have.been.calledWith('error', {message: '403: Forbidden'});
+      expect(logger.error.args[0][0]).to.have.string('Error: [AccessDeined]: ACCESS_DENIED');
+    });
+  });
+
+  it('shows a generic error page if an uncategorised error occurred', () => {
+    const logger = {error: sinon.spy()};
+    ServiceLocator.load({
+      createLogger: () => logger
+    });
+    const middleware = new ErrorHandler();
+
+    const req = {};
+    const err = new Error('UNKNOWN ERROR');
+    return promisifyExpressMiddleware(middleware, req, err).then(({res}) => {
+      expect(res.status).to.have.been.calledWith(500);
+      expect(res.render).to.have.been.calledWith('error', {message: '500: Internal Server Error'});
+      expect(logger.error.args[0][0]).to.have.string('Error: UNKNOWN');
+    });
   });
 
 });
