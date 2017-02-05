000498eb655288dec6a19f5621f1638eaed5ea5d
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-02-05 19:02:39 +1100

    Removed an unused file

--- tools/copy-done-table/dynamodb.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/aws/aws-sdk-go/service/dynamodb"
-
-type _IDynamoDBWriter interface {
-	BatchWriteItem(*dynamodb.BatchWriteItemInput) (*dynamodb.BatchWriteItemOutput, error)
-}
