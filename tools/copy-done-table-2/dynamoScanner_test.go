35fb0f40d306ad772494916ba2416a011f9fe1fb
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-02-12 16:27:56 +1100

    Provided a fake object so that I can mock the dynamo response

--- tools/copy-done-table-2/dynamoScanner_test.go
+++ tools/copy-done-table-2/dynamoScanner_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"testing"
 
+	"fmt"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
@@ -22,4 +24,38 @@ func TestScan_Integration(t *testing.T) {
 	if item := (*scanOutput).Items[0]; *(item["date"].S) != expected {
 		t.Errorf("Expected \"%s\", but got \"%s\"", expected, *(item["date"].S))
 	}
+}
+
+func TestScan(t *testing.T) {
+	dynamoClient := &_FakeDynamoDB{}
+	scanner := _Scanner{dynamoClient}
+	scanOutput, err := scanner.scan(&dynamodb.ScanInput{TableName: aws.String("TABLE_NAME")})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	expected := "DATE_VALUE"
+	if item := (*scanOutput).Items[0]; *(item["date"].S) != "DATE_VALUE" {
+		t.Errorf("Expected \"%s\", but got \"%s\"", expected, *(item["date"].S))
+	}
+}
+
+type _FakeDynamoDB struct{}
+
+func (s *_FakeDynamoDB) Scan(input *dynamodb.ScanInput) (*dynamodb.ScanOutput, error) {
+	expectedTableName := "TABLE_NAME"
+	if *input.TableName != expectedTableName {
+		return nil, fmt.Errorf("Expected \"%s\", but got \"%s\"", expectedTableName, *input.TableName)
+	}
+	fakeScanOutput := dynamodb.ScanOutput{
+		Items: []map[string]*dynamodb.AttributeValue{
+			map[string]*dynamodb.AttributeValue{
+				"date": &dynamodb.AttributeValue{
+					S: aws.String("DATE_VALUE"),
+				},
+			},
+		},
+	}
+	return &fakeScanOutput, nil
 }
