47f515ae0548bc2f89cee33d34d0181026d49d43
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-02-14 23:43:23 +1100

    DoneWrite send dynamo batchWrite request with 25 items at a time

--- tools/copy-done-table-2/doneWriter.go
+++ tools/copy-done-table-2/doneWriter.go
@@ -2,12 +2,31 @@ package main
 
 import "github.com/aws/aws-sdk-go/service/dynamodb"
 
+const _MaxBatchWriteCount = 25
+
 type _DoneWriter struct {
 	tableName           string
 	dynamoDBBatchWriter _IBatchWriter
 }
 
 func (dw *_DoneWriter) write(items *[]map[string]*dynamodb.AttributeValue) error {
+	numOfItems := len(*items)
+	for i := 0; i < numOfItems; i += _MaxBatchWriteCount {
+		var subList []map[string]*dynamodb.AttributeValue
+		if i+_MaxBatchWriteCount < numOfItems {
+			subList = (*items)[i : i+_MaxBatchWriteCount]
+		} else {
+			subList = (*items)[i:]
+		}
+		err := dw._write(&subList)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (dw *_DoneWriter) _write(items *[]map[string]*dynamodb.AttributeValue) error {
 	writeRequests := make([]*dynamodb.WriteRequest, len(*items))
 	for i, item := range *items {
 		writeRequests[i] = &dynamodb.WriteRequest{
--- tools/copy-done-table-2/doneWriter_test.go
+++ tools/copy-done-table-2/doneWriter_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestWrite_Write1Item(t *testing.T) {
-	fakeBatchWriter := _FakeBatchWriter{}
+	fakeBatchWriter := _NewFakeBatchWriter()
 	doneWriter := &_DoneWriter{
 		tableName:           "TABLE_NAME",
 		dynamoDBBatchWriter: &fakeBatchWriter,
@@ -19,7 +19,7 @@ func TestWrite_Write1Item(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	requestItems := fakeBatchWriter._input.RequestItems
+	requestItems := fakeBatchWriter._inputs[0].RequestItems
 	writeRequests := requestItems["TABLE_NAME"]
 	if writeRequests == nil {
 		t.Fatal("Expected a request to the table \"TABLE_NAME\"")
@@ -33,32 +33,74 @@ func TestWrite_Write1Item(t *testing.T) {
 	}
 }
 
-func TestWrite_Write2Item(t *testing.T) {
-	fakeBatchWriter := _FakeBatchWriter{}
+func TestWrite_Write2Items(t *testing.T) {
+	fakeBatchWriter := _NewFakeBatchWriter()
 	doneWriter := &_DoneWriter{
 		tableName:           "TABLE_NAME",
 		dynamoDBBatchWriter: &fakeBatchWriter,
 	}
-	doneItem1 := map[string]*dynamodb.AttributeValue{"SOME_KEY": &dynamodb.AttributeValue{}}
-	doneItem2 := map[string]*dynamodb.AttributeValue{"SOME_KEY": &dynamodb.AttributeValue{}}
-	doneItems := []map[string]*dynamodb.AttributeValue{doneItem1, doneItem2}
+	doneItems := createDoneItems(2)
 	err := doneWriter.write(&doneItems)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	requestItems := fakeBatchWriter._input.RequestItems
+	requestItems := fakeBatchWriter._inputs[0].RequestItems
 	writeRequests := requestItems["TABLE_NAME"]
 	if len(writeRequests) != 2 {
 		t.Fatalf("Expected 2, but got \"%d\"", len(writeRequests))
 	}
 }
 
+func TestWrite_WriteMoreThan25Items(t *testing.T) {
+	fakeBatchWriter := _NewFakeBatchWriter()
+	doneWriter := &_DoneWriter{
+		tableName:           "TABLE_NAME",
+		dynamoDBBatchWriter: &fakeBatchWriter,
+	}
+	numOfItems := 52
+	doneItems := createDoneItems(numOfItems)
+	err := doneWriter.write(&doneItems)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	requestItems1 := fakeBatchWriter._inputs[0].RequestItems
+	requestItems2 := fakeBatchWriter._inputs[1].RequestItems
+	requestItems3 := fakeBatchWriter._inputs[2].RequestItems
+	if numOfRequests := len(requestItems1["TABLE_NAME"]); numOfRequests != 25 {
+		t.Fatalf("Expected 25, but got %d", numOfRequests)
+	}
+	if numOfRequests := len(requestItems2["TABLE_NAME"]); numOfRequests != 25 {
+		t.Fatalf("Expected 25, but got %d", numOfRequests)
+	}
+	if numOfRequests := len(requestItems3["TABLE_NAME"]); numOfRequests != 2 {
+		t.Fatalf("Expected 2, but got %d", numOfRequests)
+	}
+}
+
+func createDoneItems(numOfItems int) []map[string]*dynamodb.AttributeValue {
+	items := make([]map[string]*dynamodb.AttributeValue, numOfItems)
+	for i := 0; i < numOfItems; i++ {
+		items[i] = map[string]*dynamodb.AttributeValue{"SOME_KEY": &dynamodb.AttributeValue{}}
+	}
+	return items
+}
+
 type _FakeBatchWriter struct {
-	_input *dynamodb.BatchWriteItemInput
+	_count  int
+	_inputs []*dynamodb.BatchWriteItemInput
+}
+
+func _NewFakeBatchWriter() _FakeBatchWriter {
+	return _FakeBatchWriter{
+		_count:  0,
+		_inputs: make([]*dynamodb.BatchWriteItemInput, 3),
+	}
 }
 
 func (bw *_FakeBatchWriter) BatchWriteItem(input *dynamodb.BatchWriteItemInput) (*dynamodb.BatchWriteItemOutput, error) {
-	bw._input = input
+	bw._inputs[bw._count] = input
+	bw._count++
 	return nil, nil
 }
