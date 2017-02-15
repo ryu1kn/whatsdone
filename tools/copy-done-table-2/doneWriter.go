0e66ac9eb4deb9c962a018fa2444b4abfa62bed2
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-02-16 01:07:26 +1100

    Refactoring DoneWriter#write

--- tools/copy-done-table-2/doneWriter.go
+++ tools/copy-done-table-2/doneWriter.go
@@ -10,52 +10,71 @@ type _DoneWriter struct {
 }
 
 func (dw *_DoneWriter) write(items []map[string]*dynamodb.AttributeValue) error {
-	numOfItems := len(items)
-	retryItems := make([]*dynamodb.WriteRequest, 0)
-	var maxNumOfNewItems int
-
-	for i, numOfRetryItems := 0, 0; i < numOfItems || numOfRetryItems != 0; i += maxNumOfNewItems {
-		maxNumOfNewItems = _MaxBatchWriteCount - numOfRetryItems
-		var newItems []map[string]*dynamodb.AttributeValue
-		if i+maxNumOfNewItems < numOfItems {
-			newItems = items[i : i+maxNumOfNewItems]
-		} else if i < numOfItems {
-			newItems = items[i:]
-		} else {
-			newItems = nil
+	ic := newItemCollection(items)
+	for writeRequests := ic.nextWriteRequests(); len(writeRequests) != 0; writeRequests = ic.nextWriteRequests() {
+		input := dynamodb.BatchWriteItemInput{
+			RequestItems: map[string][]*dynamodb.WriteRequest{
+				dw.tableName: writeRequests,
+			},
 		}
-		input := dw._buildBatchWriteItemInput(retryItems, newItems)
-		output, err := dw.dynamoDBBatchWriter.BatchWriteItem(input)
+		output, err := dw.dynamoDBBatchWriter.BatchWriteItem(&input)
 		if err != nil {
 			return err
 		}
-
-		retryItems = dw._extractUnprocessedRequests(output)
-		numOfRetryItems = len(retryItems)
+		ic.retryItems = dw._extractUnprocessedRequests(output)
 	}
 	return nil
 }
 
-func (dw *_DoneWriter) _buildBatchWriteItemInput(retryItems []*dynamodb.WriteRequest, newItems []map[string]*dynamodb.AttributeValue) *dynamodb.BatchWriteItemInput {
-	numOfRequests := len(retryItems) + len(newItems)
-	writeRequests := make([]*dynamodb.WriteRequest, numOfRequests)
-	for i, writeRequest := range retryItems {
-		writeRequests[i] = writeRequest
+func (dw *_DoneWriter) _extractUnprocessedRequests(output *dynamodb.BatchWriteItemOutput) []*dynamodb.WriteRequest {
+	if output.UnprocessedItems == nil {
+		return nil
 	}
-	for i, item := range newItems {
-		writeRequest := &dynamodb.WriteRequest{PutRequest: &dynamodb.PutRequest{Item: item}}
-		writeRequests[len(retryItems)+i] = writeRequest
-	}
-	return &dynamodb.BatchWriteItemInput{
-		RequestItems: map[string][]*dynamodb.WriteRequest{
-			dw.tableName: writeRequests,
-		},
+	return output.UnprocessedItems[dw.tableName]
+}
+
+func newItemCollection(items []map[string]*dynamodb.AttributeValue) itemCollection {
+	return itemCollection{
+		items:      items,
+		retryItems: []*dynamodb.WriteRequest{},
 	}
 }
 
-func (dw *_DoneWriter) _extractUnprocessedRequests(output *dynamodb.BatchWriteItemOutput) []*dynamodb.WriteRequest {
-	if output.UnprocessedItems == nil {
-		return make([]*dynamodb.WriteRequest, 0)
+type itemCollection struct {
+	items      []map[string]*dynamodb.AttributeValue
+	retryItems []*dynamodb.WriteRequest
+}
+
+func (ic *itemCollection) nextWriteRequests() []*dynamodb.WriteRequest {
+	defer func() { ic.retryItems = nil }()
+
+	var retryWriteRequest, newWriteRequests []*dynamodb.WriteRequest
+
+	if ic.retryItems == nil {
+		retryWriteRequest = []*dynamodb.WriteRequest{}
+	} else {
+		retryWriteRequest = ic.retryItems
 	}
-	return output.UnprocessedItems[dw.tableName]
+
+	numOfItems, numOfRetryItems := len(ic.items), len(ic.retryItems)
+	numOfNextNewItems := _MaxBatchWriteCount - numOfRetryItems
+	if numOfItems > numOfNextNewItems {
+		newItems := ic.items[:numOfNextNewItems]
+		ic.items = ic.items[numOfNextNewItems:]
+		newWriteRequests = ic.buildWriteRequests(newItems)
+	} else {
+		newItems := ic.items
+		ic.items = ic.items[numOfItems:]
+		newWriteRequests = ic.buildWriteRequests(newItems)
+	}
+	return append(retryWriteRequest, newWriteRequests...)
+}
+
+func (ic *itemCollection) buildWriteRequests(newItems []map[string]*dynamodb.AttributeValue) []*dynamodb.WriteRequest {
+	writeRequests := make([]*dynamodb.WriteRequest, len(newItems))
+	for i, item := range newItems {
+		writeRequest := &dynamodb.WriteRequest{PutRequest: &dynamodb.PutRequest{Item: item}}
+		writeRequests[i] = writeRequest
+	}
+	return writeRequests
 }
