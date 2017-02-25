d9dbbc3d7835fdf9cb3a6573cae0a9cc3d19fccd
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-02-25 22:54:08 +1100

    Fixed lint errors

--- modules/any-root-handler/module-config/prod.js
+++ modules/any-root-handler/module-config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "handler": "index.handler",
-  "execution_role": "arn:aws:iam::650892143834:role/service-role/write-to-dynamodb"
+  handler: 'index.handler',
+  execution_role: 'arn:aws:iam::650892143834:role/service-role/write-to-dynamodb'
 };
