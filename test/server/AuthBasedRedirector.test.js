5e8dc730c924cbfe3afe4e677ac33020748362f6
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-03-29 20:04:07 +1100

    Use mock sessionValidator in AuthBasedRedirector's test

--- test/server/AuthBasedRedirector.test.js
+++ test/server/AuthBasedRedirector.test.js
@@ -1,40 +1,36 @@
 
 const ServiceLocator = require('../../src/server/ServiceLocator');
 const AuthBasedRedirector = require('../../src/server/AuthBasedRedirector');
-const SessionValidator = require('../../src/server/SessionValidator');
 
 describe('Server AuthBasedRedirector', () => {
 
-
   it('does nothing if an authenticated user accesses any path', () => {
-    ServiceLocator.load({
-      createSessionValidator: () => new SessionValidator()
-    });
+    const sessionValidator = {validate: sinon.stub().returns(true)};
+    ServiceLocator.load({createSessionValidator: () => sessionValidator});
     const authBasedRedirector = new AuthBasedRedirector();
     const request = {path: '/PATH'};
-    const session = {isAuthorized: true};
+    const session = 'SESSION';
     const result = authBasedRedirector.redirect(request, session);
     expect(result).to.be.null;
+    expect(sessionValidator.validate).to.have.been.calledWith('SESSION');
   });
 
   it('does nothing if a non-authenticated user accesses signin page', () => {
-    ServiceLocator.load({
-      createSessionValidator: () => new SessionValidator()
-    });
+    const sessionValidator = {validate: () => false};
+    ServiceLocator.load({createSessionValidator: () => sessionValidator});
     const authBasedRedirector = new AuthBasedRedirector();
     const request = {path: '/signin'};
-    const session = {isAuthorized: false};
+    const session = 'SESSION';
     const result = authBasedRedirector.redirect(request, session);
     expect(result).to.be.null;
   });
 
   it('redirects an authenticated user to the root page if they access to the signin page', () => {
-    ServiceLocator.load({
-      createSessionValidator: () => new SessionValidator()
-    });
+    const sessionValidator = {validate: () => true};
+    ServiceLocator.load({createSessionValidator: () => sessionValidator});
     const authBasedRedirector = new AuthBasedRedirector();
     const request = {path: '/signin'};
-    const session = {isAuthorized: true};
+    const session = 'SESSION';
     const result = authBasedRedirector.redirect(request, session);
     expect(result).to.eql({
       statusCode: '303',
@@ -43,26 +39,11 @@ describe('Server AuthBasedRedirector', () => {
   });
 
   it('redirects a non-authenticated user to the singin page if they access elsewhere', () => {
-    ServiceLocator.load({
-      createSessionValidator: () => new SessionValidator()
-    });
+    const sessionValidator = {validate: () => false};
+    ServiceLocator.load({createSessionValidator: () => sessionValidator});
     const authBasedRedirector = new AuthBasedRedirector();
     const request = {path: '/PATH'};
-    const session = {isAuthorized: false};
-    const result = authBasedRedirector.redirect(request, session);
-    expect(result).to.eql({
-      statusCode: '303',
-      headers: {Location: '/signin'}
-    });
-  });
-
-  it('treats session not exists case as non-authenticated', () => {
-    ServiceLocator.load({
-      createSessionValidator: () => new SessionValidator()
-    });
-    const authBasedRedirector = new AuthBasedRedirector();
-    const request = {path: '/PATH'};
-    const session = null;
+    const session = 'SESSION';
     const result = authBasedRedirector.redirect(request, session);
     expect(result).to.eql({
       statusCode: '303',
