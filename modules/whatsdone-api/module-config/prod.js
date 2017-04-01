f8044a9066f309f4f1d4527d7a495942433b2a34
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-04-01 23:34:34 +1100

    Print lambda event and context object

--- modules/whatsdone-api/module-config/prod.js
+++ modules/whatsdone-api/module-config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
   handler: 'index.handler',
-  execution_role: 'arn:aws:iam::650892143834:role/service-role/write-to-dynamodb'
+  execution_role: 'arn:aws:iam::650892143834:role/whatsdone-app'
 };
--- modules/whatsdone-app/src/index.js
+++ modules/whatsdone-app/src/index.js
@@ -1,7 +1,7 @@
 
-'use strict';
-
 exports.handler = (event, context, callback) => {
+  console.log('JSON.stringify(event):', JSON.stringify(event));
+  console.log('JSON.stringify(context):', JSON.stringify(context));
   return callback(null, {
     statusCode: '200',
     body: '<html><head><title>TITLE</title></head><body>Hello World</body></html>',
