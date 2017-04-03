c5942c9b662fdaa83724222b47a01fcb01e2d1a8
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-04-03 21:04:49 +1000

    Deploy lambda with Env vars

--- modules/whatsdone-api/module-config/prod.js
+++ modules/whatsdone-api/module-config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  handler: 'index.handler',
-  execution_role: 'arn:aws:iam::650892143834:role/service-role/write-to-dynamodb'
+  executionRole: 'arn:aws:iam::650892143834:role/whatsdone-app',
+  sessionSecret: 'encrypted::value::AQECAHhxEKFjg5fr5fP9ZzhAmP1EW3Z4kDBqGx2ToIRPQ85N5gAAAHYwdAYJKoZIhvcNAQcGoGcwZQIBADBgBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDPJlsj7IgQ04iCCgiQIBEIAzlWEEkfi7+/IlOuHXkzLqims8Iep/E/q6tD0cBi8fKlm+F7ImJY4Nr7v9jnmGzgRqPG4z,provider::kms,keyId::alias/whatsdone-app,region::ap-southeast-2'
 };
