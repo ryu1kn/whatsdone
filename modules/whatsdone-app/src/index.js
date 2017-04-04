4cd89ef4e60d413f6d8062e8dfa9c9b97a4cd711
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-04-04 23:12:21 +1000

    Removed sample handler

--- modules/whatsdone-app/src/index.js
+++ modules/whatsdone-app/src/index.js
@@ -4,16 +4,6 @@ const ServiceFactory = require('./lib/ServiceFactory');
 
 ServiceLocator.load(new ServiceFactory({env: process.env}));
 
-exports.handler = (event, context, callback) => {
-  return callback(null, {
-    statusCode: '200',
-    body: '<html><head><title>TITLE</title></head><body>Hello World</body></html>',
-    headers: {
-      'Content-Type': 'text/html'
-    }
-  });
-};
-
 exports.getRootPageRequestHandler = ServiceLocator.getRootPageRequestHandler;
 
 exports.getSigninRequestHandler = ServiceLocator.getSigninRequestHandler;
