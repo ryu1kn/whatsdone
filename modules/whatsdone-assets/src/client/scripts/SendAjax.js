9f009d7c0b22675d2c45bd4775cd8863ad978759
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-07-01 13:26:58 +1000

    Signin html page post login information directly to api endpoint

--- modules/whatsdone-assets/src/client/scripts/SendAjax.js
+++ modules/whatsdone-assets/src/client/scripts/SendAjax.js
@@ -2,20 +2,29 @@
 const DEFAULT_HEADERS = {
   'Accept-Encoding': 'gzip, deflate'
 };
-const StatusCode = {
-  SEE_OTHER: 303
-};
 
 module.exports = (url, options = {}) => {
   const headers = Object.assign({}, options.headers, DEFAULT_HEADERS);
   const finalOptions = Object.assign({}, options, {headers});
-  return fetch(url, finalOptions).then(parse);
+  return fetch(url, finalOptions)
+    .then(response => parse(response, getParseOptions(options)));
 };
 
-function parse(response) {
-  if (response.status === StatusCode.SEE_OTHER) {
-    window.location = '/signin';
-  }
+function parse(response, parseOptions) {
   const contentType = response.headers.get('Content-Type').toLowerCase();
+  if (shouldLogin(contentType, parseOptions)) {
+    window.location = '/signin.html';
+  }
   return contentType === 'application/json' ? response.json() : response.text();
+}
+
+function getParseOptions(options) {
+  return {
+    expectedContentType: (options.expectedContentType || '').toLowerCase()
+  };
+}
+
+function shouldLogin(contentType, parseOptions) {
+  return parseOptions.expectedContentType &&
+      parseOptions.expectedContentType !== contentType;
 }
