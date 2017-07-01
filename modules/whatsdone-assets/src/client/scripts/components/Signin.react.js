844b396c3b22b7d79070c1efccfa5988842a1287
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-07-01 21:08:37 +1000

    No component state, form element has state

--- modules/whatsdone-assets/src/client/scripts/components/Signin.react.js
+++ modules/whatsdone-assets/src/client/scripts/components/Signin.react.js
@@ -6,22 +6,8 @@ class Signin extends React.Component {
 
   constructor(params) {
     super(params);
-    console.log('initializing sigin page');
 
-    this.state = {
-      email: '',
-      password: '',
-      rememberMe: true
-    };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({[target.name]: value});
   }
 
   handleSubmit(event) {
@@ -38,11 +24,11 @@ class Signin extends React.Component {
         <form className="form-signin" onSubmit={this.handleSubmit}>
           <h2 className="form-signin-heading">Please sign in</h2>
           <label className="sr-only" htmlFor="inputEmail">Email address</label>
-          <input className="form-control" type="email" id="inputEmail" name="email" placeholder="Email address" value={this.state.email} onChange={this.handleInputChange} required autoFocus />
+          <input className="form-control" type="email" id="inputEmail" name="email" placeholder="Email address" required autoFocus />
           <label className="sr-only" htmlFor="inputPassword">Password</label>
-          <input className="form-control" type="password" id="inputPassword" name="password" placeholder="Password" value={this.state.password} onChange={this.handleInputChange} required />
+          <input className="form-control" type="password" id="inputPassword" name="password" placeholder="Password" required />
           <div className="checkbox">
-            <label><input type="checkbox" name="rememberMe" checked={this.state.isGoing} onChange={this.handleInputChange} />Remember me</label>
+            <label><input type="checkbox" name="rememberMe" />Remember me</label>
           </div>
           <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
         </form>
