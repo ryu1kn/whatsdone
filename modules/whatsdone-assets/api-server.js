9d5ca0c72b09de1e44aff73c215ff0c7fe0b3332
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-07-02 19:48:06 +1000

    Forgot to specify credentials option for express

--- modules/whatsdone-assets/api-server.js
+++ modules/whatsdone-assets/api-server.js
@@ -5,6 +5,7 @@ const app = express();
 
 var corsOptions = {
   origin: 'http://localhost:8080',
+  credentials: true,
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
