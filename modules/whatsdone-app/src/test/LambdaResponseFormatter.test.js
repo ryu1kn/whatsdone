8c821d354242cf328c3245d81574480b12afd8cc
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-07-02 16:05:48 +1000

    Fixed LambdaResponseFormatter test...

--- modules/whatsdone-app/src/test/LambdaResponseFormatter.test.js
+++ modules/whatsdone-app/src/test/LambdaResponseFormatter.test.js
@@ -1,13 +1,30 @@
 
+const ServiceLocator = require('../lib/ServiceLocator');
 const LambdaResponseFormatter = require('../lib/LambdaResponseFormatter');
 
 describe('Server LambdaResponseFormatter', () => {
 
   it('passes through as standard response format IS lambda\'s response format', () => {
+    const config = {webappOrigin: 'WEBAPP_ORIGIN'};
+    ServiceLocator.load({
+      createConfig: () => config
+    });
     const formatter = new LambdaResponseFormatter();
 
-    const response = 'STANDARD_RESPONSE';
-    expect(formatter.format(response)).to.eql('STANDARD_RESPONSE');
+    const response = {
+      headers: {'X-ANY-HEADER': '..'},
+      NON_HEADER_VALUES: '..'
+    };
+    expect(formatter.format(response)).to.eql({
+      headers: {
+        'X-ANY-HEADER': '..',
+        'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Origin': 'WEBAPP_ORIGIN'
+      },
+      NON_HEADER_VALUES: '..'
+    });
   });
 
 });
