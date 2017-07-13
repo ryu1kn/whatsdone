2ffffa817bd3bd5e1bec2fef0eac3d42313cf91a
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-07-13 19:10:27 +1000

    Get dones command use API defined in config

--- tools/api-client/src/GetDonesCommand.hs
+++ tools/api-client/src/GetDonesCommand.hs
@@ -3,18 +3,26 @@
 
 module GetDonesCommand where
 
+import Data.Text
 import Network.HTTP.Simple
 import ApiClientArgs
+import AppConfig
 
-apiEndpoint = "https://whatsdone-api.ryuichi.io"
 sessionFile = "__session.txt"
 
 getDones :: Options -> IO ()
 getDones opts = do
-    requestDones "connect.sid=s%3A3fc5d5c8-6f50-4da3-b361-854456cad538.W3xzaA6XPAgLhuPPxKy97tE1c0BQBkLvZLTRfRYfVyo"
+    d <- loadConfig $ optConfig opts
+    case d of
+        Left  err    -> putStrLn err
+        Right config -> getDones_ config
 
-requestDones sessionId = do
-    initReq <- parseRequest $ apiEndpoint ++ "/dones"
+getDones_ :: AppConfig -> IO ()
+getDones_ config = do
+    requestDones config "connect.sid=s%3A3fc5d5c8-6f50-4da3-b361-854456cad538.W3xzaA6XPAgLhuPPxKy97tE1c0BQBkLvZLTRfRYfVyo"
+
+requestDones config sessionId = do
+    initReq <- parseRequest $ unpack (apiEndpoint config) ++ "/dones"
     let req = addRequestHeader "Cookie" sessionId initReq
     res <- httpJSON req
     putStrLn $ getResponseBody res
