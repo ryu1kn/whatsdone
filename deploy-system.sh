9aee4013cdfbaf742442c0d1f504d402123308bf
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-07-15 20:43:19 +1000

    Place env independent/dependent modules in different dirs

--- deploy-system.sh
+++ deploy-system.sh
@@ -2,10 +2,17 @@
 
 set -euo pipefail
 
-MODULES=(
-    `cat ./module-deploy-order.txt`
+MODULES__NO_ENV=(
+    `cat ./module-deploy-order--no-env.txt`
+)
+MODULES__ENV=(
+    `cat ./module-deploy-order--env.txt`
 )
 
-for MODULE in ${MODULES[*]} ; do
-    (cd modules/$MODULE_NAME && npm run deploy -- --env $ENV_NAME --region ap-southeast-2)
+for MODULE in ${MODULES__NO_ENV[*]} ; do
+    (cd modules/no-env/$MODULE_NAME && npm run deploy -- --region ap-southeast-2)
+done
+
+for MODULE in ${MODULES__ENV[*]} ; do
+    (cd modules/env/$MODULE_NAME && npm run deploy -- --env $ENV_NAME --region ap-southeast-2)
 done
--- destroy-system.sh
+++ destroy-system.sh
@@ -2,10 +2,10 @@
 
 set -euo pipefail
 
-MODULES=(
-    `cat ./module-deploy-order.txt | tail -r`
+MODULES__ENV=(
+    `cat ./module-deploy-order--env.txt | tail -r`
 )
 
-for MODULE in ${MODULES[*]} ; do
-    (cd modules/$MODULE_NAME && npm run destroy -- --env $ENV_NAME --region ap-southeast-2)
+for MODULE in ${MODULES__ENV[*]} ; do
+    (cd modules/env/$MODULE_NAME && npm run destroy -- --env $ENV_NAME --region ap-southeast-2)
 done
