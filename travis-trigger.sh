897b5418dc96c54dcacd73dde5e339cd596e2455
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-07-16 15:02:32 +1000

    Give build number to build tasks

--- travis-trigger.sh
+++ travis-trigger.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 if [ "$TRAVIS_EVENT_TYPE" = "cron" ] ; then
-    ENV_NAME=sit npm run deploy-system
+    CI_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER ENV_NAME=sit npm run deploy-system
 else
-    npm run build
+    CI_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER npm run build
 fi
