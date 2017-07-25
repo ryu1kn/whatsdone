470c3be6b551c34b404c74e49af0d30a17ebf74a
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-07-26 00:38:04 +1000

    Improve mock api to receive post done request

--- modules/env/whatsdone-assets/app/api-server.js
+++ modules/env/whatsdone-assets/app/api-server.js
@@ -1,31 +1,55 @@
 
 const express = require('express');
+const querystring = require('querystring');
 const cors = require('cors');
 const app = express();
 
-var corsOptions = {
+const corsOptions = {
   origin: 'http://localhost:8080',
   credentials: true,
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
+const dones = dummyDones();
+
 app.get('/dones', cors(corsOptions), (req, res) => {
-  res.status(200).json(dummyDones());
+  res.status(200).json(dones);
+});
+
+app.post('/dones', cors(corsOptions), (req, res) => {
+  promiseToGetBody(req)
+    .then(bodyString => {
+      const parsedBody = querystring.parse(bodyString);
+      dones.push(Object.assign({}, parsedBody, {id: generateDummyId()}));
+      res.status(202).json(dones);
+    })
+    .catch(e => console.error(e.stack));
 });
 
 app.post('/signin', cors(corsOptions), (req, res) => {
   res.sendStatus(200);
 });
 
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
   res.sendStatus(404);
 });
 
-var PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => {
   console.log(`Production Express server running at localhost: ${PORT}`);
 });
+
+function promiseToGetBody(req) {
+  return new Promise((resolve, reject) => {
+    const tmp = {string: ''};
+    req.on('data', data => {
+      tmp.string += data.toString();
+    });
+    req.on('end', () => resolve(tmp.string));
+    req.on('error', e => reject(e));
+  });
+}
 
 function dummyDones() {
   return [
@@ -44,4 +68,8 @@ function dummyDones() {
       id: 'YYYYYYYY-YYYY-YYYY-YYYY-YYYYYYYYYYYY'
     }
   ];
+}
+
+function generateDummyId() {
+  return `DUMMY_ID_${new Date().getTime()}`;
 }
