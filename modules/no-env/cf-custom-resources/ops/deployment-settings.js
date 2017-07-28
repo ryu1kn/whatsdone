f620f5a223bf3958df8415c47194facc154979d7
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-07-28 20:40:07 +1000

    Converted cf-custom-resources module's deploy settings

--- modules/no-env/cf-custom-resources/ops/deployment-settings.js
+++ modules/no-env/cf-custom-resources/ops/deployment-settings.js
@@ -4,6 +4,12 @@ module.exports = {
   config: {
     script: "node -p \"JSON.stringify(require('../module-config.js'))\""
   },
+  outputsStore: {
+    type: 's3-bucket',
+    region: {$ref: '#/_args/region'},
+    bucket: 'whatsdone-deployment-outputs',
+    prefix: 'cf-custom-resources'
+  },
   tasks: [
     {
       id: 'build-upload-path',
@@ -19,13 +25,13 @@ module.exports = {
     {
       id: 'deploy-lambda',
       type: 'cf-stack',
-      stackName: 'module',
+      stackName: 'cf-custom-resources-module',
       stackTemplate: {
         script: 'cp ./template.json $KUMO_TEMPLATE_OUTPUT_FILE'
       },
       stackParams: {
         S3Bucket: {$ref: '#/_deploymentConfig/artifactBucket'},
-        S3Key: {$ref: '#/_deploymentOutputs/cf-custom-resources/S3Key'}
+        S3Key: {$ref: '#/_deploymentOutputs/S3Key'}
       }
     }
   ]
