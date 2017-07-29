6ed3700ea849568b8e17e5a4dca959b1bfb62c25
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-07-29 22:58:15 +1000

    Execute command in task section

--- tools/buildman/index.js
+++ tools/buildman/index.js
@@ -1,2 +1,8 @@
 
-console.log('Hi, I\'m buildman');
+const path = require('path');
+
+module.exports = ({currentDirectory, execSync}) => {
+  const configPath = path.join(currentDirectory, 'buildman.config.js');
+  const config = require(configPath);
+  execSync(config.tasks[0].command);
+};
--- tools/buildman/test/integration/index.test.js
+++ tools/buildman/test/integration/index.test.js
@@ -1,12 +1,31 @@
 
 const test = require('tape');
-const {exec} = require('child_process');
+const {execSync} = require('child_process');
+const fs = require('fs');
 
-test('it executes a command', t => {
+const TEST_DIR = __dirname;
+
+beforeAll();
+
+test('it executes a task', t => {
   t.plan(1);
 
-  const options = {cwd: __dirname};
-  exec('../../bin/buildman', options, (error, stdout, stderr) => {
-    t.equal(stdout, 'Hi, I\'m buildman\n');
-  });
+  const buildmanConfig = `
+module.exports = {
+  tasks: [
+    {
+      command: 'mkdir -p tmp && echo Hello World! > tmp/hello.txt'
+    }
+  ]
+};`;
+  fs.writeFileSync(`${TEST_DIR}/buildman.config.js`, buildmanConfig, 'utf8');
+
+  execSync('../../bin/buildman', {cwd: TEST_DIR});
+  const commandOutput = fs.readFileSync(`${TEST_DIR}/tmp/hello.txt`, 'utf8');
+  t.equal(commandOutput, 'Hello World!\n');
 });
+
+function beforeAll() {
+  execSync('rm -rf tmp', {cwd: TEST_DIR});
+  execSync('rm -rf buildman.config.js', {cwd: TEST_DIR});
+}
