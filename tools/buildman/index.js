8adcd35ac73b43e63cd6df4f8c6a88df365f856a
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-07-29 23:41:35 +1000

    Refactoring

--- tools/buildman/index.js
+++ tools/buildman/index.js
@@ -1,12 +1,20 @@
 
-const path = require('path');
-
-module.exports = ({currentDirectory, execSync, input}) => {
-  const configPath = path.join(currentDirectory, 'buildman.config.js');
-  const config = require(configPath);
+module.exports = async ({config, execSync, stdin}) => {
+  const input = await readWholeInput(stdin);
   const filePaths = input.split('\n');
   config.tasks.forEach(task => {
     if (!task.path) return execSync(task.command);
     if (filePaths.includes(task.path)) return execSync(task.command);
   });
 };
+
+function readWholeInput(stdin) {
+  let input = '';
+  stdin.on('data', data => {
+    input += data.toString();
+  });
+  return new Promise((resolve, reject) => {
+    stdin.on('error', e => reject(e));
+    stdin.on('close', () => resolve(input));
+  });
+}
