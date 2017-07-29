b3bf7b3a3baa5d1c06d302ae2ee92797805cdad7
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-07-29 23:32:05 +1000

    Execute commands that match path patterns

--- tools/buildman/index.js
+++ tools/buildman/index.js
@@ -1,8 +1,12 @@
 
 const path = require('path');
 
-module.exports = ({currentDirectory, execSync}) => {
+module.exports = ({currentDirectory, execSync, input}) => {
   const configPath = path.join(currentDirectory, 'buildman.config.js');
   const config = require(configPath);
-  execSync(config.tasks[0].command);
+  const filePaths = input.split('\n');
+  config.tasks.forEach(task => {
+    if (!task.path) return execSync(task.command);
+    if (filePaths.includes(task.path)) return execSync(task.command);
+  });
 };
--- tools/buildman/test/integration/index.test.js
+++ tools/buildman/test/integration/index.test.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 
 const TEST_DIR = __dirname;
 
-beforeAll();
-
 test('it executes a task', t => {
   t.plan(1);
+
+  clean();
 
   const buildmanConfig = `
 module.exports = {
@@ -25,7 +25,55 @@ module.exports = {
   t.equal(commandOutput, 'Hello World!\n');
 });
 
-function beforeAll() {
+test('it executes multiple tasks', t => {
+  t.plan(1);
+
+  clean();
+
+  const buildmanConfig = `
+module.exports = {
+  tasks: [
+    {
+      command: 'mkdir -p tmp && echo task1 >> tmp/tasks.txt'
+    },
+    {
+      command: 'mkdir -p tmp && echo task2 >> tmp/tasks.txt'
+    }
+  ]
+};`;
+  fs.writeFileSync(`${TEST_DIR}/buildman.config.js`, buildmanConfig, 'utf8');
+
+  execSync('../../bin/buildman', {cwd: TEST_DIR});
+  const commandOutput = fs.readFileSync(`${TEST_DIR}/tmp/tasks.txt`, 'utf8');
+  t.equal(commandOutput, 'task1\ntask2\n');
+});
+
+test('it executes tasks that match path patterns', t => {
+  t.plan(1);
+
+  clean();
+
+  const buildmanConfig = `
+module.exports = {
+  tasks: [
+    {
+      path: 'dir1/test.txt',
+      command: 'mkdir -p tmp && echo task1 >> tmp/tasks.txt'
+    },
+    {
+      path: 'dir2/test.txt',
+      command: 'mkdir -p tmp && echo task2 >> tmp/tasks.txt'
+    }
+  ]
+};`;
+  fs.writeFileSync(`${TEST_DIR}/buildman.config.js`, buildmanConfig, 'utf8');
+
+  execSync('echo dir2/test.txt | ../../bin/buildman', {cwd: TEST_DIR});
+  const commandOutput = fs.readFileSync(`${TEST_DIR}/tmp/tasks.txt`, 'utf8');
+  t.equal(commandOutput, 'task2\n');
+});
+
+function clean() {
   execSync('rm -rf tmp', {cwd: TEST_DIR});
   execSync('rm -rf buildman.config.js', {cwd: TEST_DIR});
 }
