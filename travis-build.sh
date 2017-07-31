7c485d55cf379b9ea3f367f04bb923577c412cd0
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-07-31 23:56:37 +1000

    Print out more information

--- travis-build.sh
+++ travis-build.sh
@@ -2,4 +2,9 @@
 
 set -euo pipefail
 
+echo Commit range: $TRAVIS_COMMIT_RANGE
+
+echo List of changed file(s) in the range:
+git diff --name-only $TRAVIS_COMMIT_RANGE
+
 git diff --name-only $TRAVIS_COMMIT_RANGE | BUILD_NUMBER=$TRAVIS_BUILD_NUMBER ./node_modules/.bin/buildman
