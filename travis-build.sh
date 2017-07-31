a7834229dc8a29bd578eb743a82de22dffe8b8d6
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-08-01 00:00:44 +1000

    Need to quote...

--- travis-build.sh
+++ travis-build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 echo Commit range: $TRAVIS_COMMIT_RANGE
 
-echo List of changed file(s) in the range:
+echo "List of changed file(s) in the range:"
 git diff --name-only $TRAVIS_COMMIT_RANGE
 
 git diff --name-only $TRAVIS_COMMIT_RANGE | BUILD_NUMBER=$TRAVIS_BUILD_NUMBER ./node_modules/.bin/buildman
