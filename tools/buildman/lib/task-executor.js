a43c3a33586d2cdac4ab07b08ee145aa06d9ed42
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-08-02 01:46:32 +1000

    Refactoring. Extracted TaskExecutor

---
+++ tools/buildman/lib/task-executor.js
@@ -0,0 +1,38 @@
+
+const PATH_VAR_ENV_PREFIX = 'BM_PATH_VAR_';
+
+class TaskExecutor {
+
+  constructor(params) {
+    this._execSync = params.execSync;
+    this._envVars = params.envVars;
+    this._logger = params.logger;
+  }
+
+  execute({task, pathVarSet}) {
+    const executionPlans = pathVarSet.map(pathVars => ({
+      command: task.command,
+      envVars: this._buildEnvVars(pathVars)
+    }));
+    executionPlans.forEach(executionPlan => {
+      this._execute(executionPlan.command, executionPlan.envVars);
+    });
+  }
+
+  _execute(command, additionalEnvVars) {
+    this._logger.log(command);
+    this._execSync(command, {
+      env: Object.assign({}, this._envVars, additionalEnvVars)
+    });
+  }
+
+  _buildEnvVars(pathVars) {
+    return pathVars.reduce((result, value, i) => {
+      const pathVarEnvName = PATH_VAR_ENV_PREFIX + (i + 1);
+      return Object.assign({}, result, {[pathVarEnvName]: value});
+    }, Object.create(null));
+  }
+
+}
+
+module.exports = TaskExecutor;
--- tools/buildman/lib/task-list-executor.js
+++ tools/buildman/lib/task-list-executor.js
@@ -1,44 +1,24 @@
 
 const PathVarSetCollectorFactory = require('./path-var-set-collector-factory');
-const pathVarSetCollectorFactory = new PathVarSetCollectorFactory();
+const TaskExecutor = require('./task-executor');
 
 class TaskListExecutor {
 
   constructor(params) {
-    this._execSync = params.execSync;
-    this._envVars = params.envVars;
-    this._logger = params.logger;
-  }
-
-  execute({tasks, filePaths}) {
-    tasks
-      .map(task => {
-        const collector = pathVarSetCollectorFactory.create(task.path);
-        const pathVarSet = collector.collect(filePaths);
-        return pathVarSet.map(pathVars => ({
-          command: task.command,
-          envVars: this._buildEnvVars(pathVars)
-        }));
-      })
-      .reduce((result, executionPlans) => [...result, ...executionPlans], [])
-      .forEach(executionPlan => {
-        this._execute(executionPlan.command, executionPlan.envVars);
-      });
-  }
-
-  _execute(command, additionalEnvVars) {
-    this._logger.log(command);
-    this._execSync(command, {
-      env: Object.assign({}, this._envVars, additionalEnvVars)
+    this._pathVarSetCollectorFactory = new PathVarSetCollectorFactory();
+    this._taskExecutor = new TaskExecutor({
+      execSync: params.execSync,
+      envVars: params.envVars,
+      logger: params.logger
     });
   }
 
-  _buildEnvVars(pathVars) {
-    return pathVars.reduce((result, value, i) => {
-      return Object.assign({}, result, {
-        [`BM_PATH_VAR_${i + 1}`]: value
-      });
-    }, Object.create(null));
+  execute({tasks, filePaths}) {
+    tasks.forEach(task => {
+      const collector = this._pathVarSetCollectorFactory.create(task.path);
+      const pathVarSet = collector.collect(filePaths);
+      this._taskExecutor.execute({task, pathVarSet});
+    });
   }
 
 }
