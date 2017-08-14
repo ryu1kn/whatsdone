8edae45fa39cc4bc0a53d4050f7400340a191b0b
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-08-14 21:54:56 +1000

    Use dynamo table provided by done-store module

--- modules/whatsdone-api/module-config/sit.js
+++ modules/whatsdone-api/module-config/sit.js
@@ -4,7 +4,6 @@ const projectConfig = require('../../../config/sit');
 module.exports = {
   domainName: 'whatsdone-sit-api.ryuichi.io',
   hostedZoneId: projectConfig.hostedZoneId,
-  doneTableName: 'whatsdone-dones',
   userTableName: 'whatsdone-users',
   sessionTableName: 'whatsdone-sessions',
   sessionSecret: 'encrypted::value::AQECAHhxEKFjg5fr5fP9ZzhAmP1EW3Z4kDBqGx2ToIRPQ85N5gAAAHYwdAYJKoZIhvcNAQcGoGcwZQIBADBgBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDPJlsj7IgQ04iCCgiQIBEIAzlWEEkfi7+/IlOuHXkzLqims8Iep/E/q6tD0cBi8fKlm+F7ImJY4Nr7v9jnmGzgRqPG4z,provider::kms,keyId::alias/whatsdone-app,region::ap-southeast-2'
