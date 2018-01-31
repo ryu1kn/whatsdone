3216065bd1bdc70075743cbc9b338f7f33dd863b
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-01-31 20:51:07 +1100

    Wrote tests for UserIdRepository

---
+++ modules/whatsdone-app/src/test/repositories/UserId.test.js
@@ -0,0 +1,75 @@
+
+const ServiceLocator = require('../../lib/ServiceLocator');
+const UserIdRepository = require('../../lib/repositories/UserId');
+
+describe('Server UserIdRepository', () => {
+
+  it('queries user id database with given id', () => {
+    const dynamoDBDocumentClient = createDynamoDBDocumentClientForGet();
+    const repository = createUserIdRepository(dynamoDBDocumentClient);
+    return repository.getCognitoUserId('USER_ID').then(() => {
+      expect(dynamoDBDocumentClient.get).to.have.been.calledWith({
+        TableName: 'USER_ID_TABLE_NAME',
+        Key: {id: 'USER_ID'}
+      });
+    });
+  });
+
+  it('finds a cognito user id', () => {
+    const dynamoDBDocumentClient = createDynamoDBDocumentClientForGet();
+    const repository = createUserIdRepository(dynamoDBDocumentClient);
+    return repository.getCognitoUserId('USER_ID').then(result => {
+      expect(result).to.eql('COGNITO_USER_ID');
+    });
+  });
+
+  it('searches on user id database for old user id with cognito user id', () => {
+    const dynamoDBDocumentClient = createDynamoDBDocumentClientForQuery();
+    const repository = createUserIdRepository(dynamoDBDocumentClient);
+    return repository.getByCognitoUserId('COGNITO_USER_ID').then(() => {
+      expect(dynamoDBDocumentClient.query).to.have.been.calledWith({
+        TableName: 'USER_ID_TABLE_NAME',
+        IndexName: 'cognitoUserId',
+        KeyConditionExpression: 'cognitoUserId = :hkey',
+        ExpressionAttributeValues: {':hkey': 'COGNITO_USER_ID'},
+        ProjectionExpression: 'id'
+      });
+    });
+  });
+
+  it('finds a user id', () => {
+    const dynamoDBDocumentClient = createDynamoDBDocumentClientForQuery();
+    const repository = createUserIdRepository(dynamoDBDocumentClient);
+    return repository.getByCognitoUserId('COGNITO_USER_ID').then(result => {
+      expect(result).to.eql('OLD_USER_ID');
+    });
+  });
+
+  function createDynamoDBDocumentClientForGet() {
+    return {
+      get: sinon.stub().returns({
+        promise: () => Promise.resolve({
+          Item: {cognitoUserId: 'COGNITO_USER_ID'}
+        })
+      })
+    };
+  }
+
+  function createDynamoDBDocumentClientForQuery() {
+    return {
+      query: sinon.stub().returns({
+        promise: () => Promise.resolve({
+          Items: [{id: 'OLD_USER_ID'}]
+        })
+      })
+    };
+  }
+
+  function createUserIdRepository(dynamoDBDocumentClient) {
+    ServiceLocator.load({
+      createDynamoDBDocumentClient: () => dynamoDBDocumentClient
+    });
+    return new UserIdRepository({tableName: 'USER_ID_TABLE_NAME'});
+  }
+
+});
