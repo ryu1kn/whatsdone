a2c48ae5595a314db481fc39de527272d61a2a95
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-02-04 17:42:16 +1100

    Trigger whatsdone-assets module build

--- modules/whatsdone-assets/upload-artifact.js
+++ modules/whatsdone-assets/upload-artifact.js
@@ -4,12 +4,12 @@ if (!process.env.BUILD_NUMBER) {
   throw new Error('Environment variable BUILD_NUMBER is not set');
 }
 
-const config = require('./module-config/common');
+const {artifactBucket, artifactBasePath} = require('./module-config/common');
 
 const params = {
   directoryPath: process.env.npm_package_config_APP_BUILD_DIR,
-  s3bucket: config.artifactBucket,
-  s3key: `${config.artifactBasePath}/${process.env.BUILD_NUMBER}.zip`
+  s3bucket: artifactBucket,
+  s3key: `${artifactBasePath}/${process.env.BUILD_NUMBER}.zip`
 };
 uploadDirectory(params).catch(e => {
   setTimeout(() => {
