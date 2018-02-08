7956f04c2093f0347dd8457129ddee1a5b3ad24c
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-02-08 23:57:20 +1100

    Read deployment output location from config

--- config/common.js
+++ config/common.js
@@ -1,5 +1,6 @@
 
 module.exports = {
+  deploymentOutputsBucket: 'whatsdone-deployment-outputs',
   artifactBucket: 'whatsdone-artifacts',
   hostedZoneId: 'Z1ZSF9275QJEQQ'
 };
---
+++ modules/whatsdone-app/module-config.js
@@ -0,0 +1,3 @@
+const { deploymentOutputsBucket } = require('../../config/common')
+
+module.exports = { deploymentOutputsBucket }
