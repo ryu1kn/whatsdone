7460c4dab919efcdc0707499a395bc11548e67c2
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-06-17 15:35:11 +1000

    Extracted Router class from lambda's entry point

---
+++ modules/whatsdone-app/src/lib/Router.ts
@@ -0,0 +1,67 @@
+import Route = require('route-parser');
+import LambdaRequestHandler from './LambdaRequestHandler';
+import {ObjectMap} from './models/Collection';
+import {Event} from './models/Lambda';
+import {Response} from './models/Request';
+
+export type RouteDefinition = {
+  handler: LambdaRequestHandler['handle'],
+  pattern: Route
+};
+
+export class Router {
+  private _handlers: ObjectMap<RouteDefinition[]>;
+
+  constructor() {
+    this._handlers = {
+      get: [],
+      post: [],
+      delete: [],
+      put: []
+    };
+  }
+
+  async route(event: Event): Promise<Response> {
+    const method = event.httpMethod.toLowerCase();
+    const handlerItem = this._handlers[method].find(
+      handlerItem => !!handlerItem.pattern.match(event.path)
+    );
+    if (handlerItem) {
+      const pathParameters = handlerItem.pattern.match(event.path);
+      const finalEvent = Object.assign({}, event, {pathParameters});
+      return handlerItem.handler(finalEvent);
+    }
+    return {
+      statusCode: '404',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        errors: [
+          {title: '404: Not Found'}
+        ]
+      })
+    };
+  }
+
+  get(pattern, handler) {
+    this.registerHandler('get', pattern, handler);
+  }
+
+  post(pattern, handler) {
+    this.registerHandler('post', pattern, handler);
+  }
+
+  delete(pattern, handler) {
+    this.registerHandler('delete', pattern, handler);
+  }
+
+  put(pattern, handler) {
+    this.registerHandler('put', pattern, handler);
+  }
+
+  private registerHandler(method, pattern, handler) {
+    this._handlers[method].push({
+      pattern: new Route(pattern),
+      handler
+    });
+  }
+}
