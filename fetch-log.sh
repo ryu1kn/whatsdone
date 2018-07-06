5354a5b3c60fc87b0fc8760bf574aeda52c48cef
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-07-06 21:50:29 +1000

    fetch-log script now receive options instead of positional args

--- fetch-log.sh
+++ fetch-log.sh
@@ -3,9 +3,33 @@
 set -euo pipefail
 
 log_group=/aws/lambda/whatsdone-prod-ProxyLambdaFunction
+log_entry_limit=20
+usage_message=$(cat << EOF
+
+  Description:  Query CloudWatch logs.
+
+  Usage:        $0 [options...]
+
+  Options:
+    -l log_group        Log group name. Default: $log_group
+    -e log_entry_limit  Log entry limit. Default: $log_entry_limit
+    -h                  Help. Print this usage information.
+
+EOF
+)
+
+while [ $# -gt 0 ] ; do
+    case $1 in
+        -l) log_group="$2"; shift ;;
+        -e) log_entry_limit="$2"; shift ;;
+        -h) echo "$usage_message" ; exit 0 ;;
+         *) break
+    esac
+    shift
+done
+
 log_stream=`aws logs describe-log-streams --log-group-name $log_group \
     | jq '.logStreams | map(.logStreamName) | sort | reverse | .[0]' --raw-output`
-log_entry_limit=${1:-20}
 
 aws logs get-log-events \
     --limit $log_entry_limit \
