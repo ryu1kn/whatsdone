bcff26c127586dcf55f517b98fcf1482294dfa31
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-07-08 13:40:43 +1000

    Replaced sinon with testdouble in UpdateDoneRequestProcessor test

--- modules/whatsdone-app/src/test/request-processors/UpdateDone.test.ts
+++ modules/whatsdone-app/src/test/request-processors/UpdateDone.test.ts
@@ -1,36 +1,32 @@
+import {expect} from 'chai';
+import * as td from 'testdouble';
 import UpdateDoneRequestProcessor from '../../lib/request-processors/UpdateDone';
 import ServiceLocator from '../../lib/ServiceLocator';
-import {expect} from 'chai';
-import sinon = require('sinon');
 import ServiceFactory from '../../lib/ServiceFactory';
 import {request, session} from '../helper/NormalisedRequestData';
+import UpdateDoneCommand from '../../lib/commands/UpdateDone';
 
 describe('Server UpdateDoneRequestProcessor', () => {
 
-  it('bridges http request/response pair to UpdateDone command', () => {
-    const updateDoneCommand = {execute: sinon.stub().returns(Promise.resolve({UPDATED_DONE_ITEM: '..'}))};
+  it('bridges http request/response pair to UpdateDone command', async () => {
+    const doneDiff = {doneThing: '..'};
+    const updateDoneCommand = td.object('execute') as UpdateDoneCommand;
+    td.when(updateDoneCommand.execute(doneDiff, 'DONE_ID', 'USER_ID'))
+      .thenResolve({UPDATED_DONE_ITEM: '..'});
     ServiceLocator.load({
       createUpdateDoneCommand: () => updateDoneCommand
     } as ServiceFactory);
     const processor = new UpdateDoneRequestProcessor();
 
-    const req = Object.assign({}, request, {
-      body: {DONE_DATA: '..'}
-    });
-    return processor.process(req, session).then(response => {
-      expect(response).to.eql({
-        statusCode: '200',
-        headers: {
-          'Content-Type': 'application/json',
-          'Cache-Control': 'no-cache'
-        },
-        body: '{"UPDATED_DONE_ITEM":".."}'
-      });
-      expect(updateDoneCommand.execute.args[0]).to.eql([
-        {DONE_DATA: '..'},
-        'DONE_ID',
-        'USER_ID'
-      ]);
+    const req = Object.assign({}, request, {body: doneDiff});
+    const response = await processor.process(req, session);
+    expect(response).to.eql({
+      statusCode: '200',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache'
+      },
+      body: '{"UPDATED_DONE_ITEM":".."}'
     });
   });
 
