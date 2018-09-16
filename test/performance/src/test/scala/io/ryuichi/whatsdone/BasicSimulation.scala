638ba51a3c5fdf41909621fefda399519ca6cf2d
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-09-16 21:26:57 +1000

    Fix that the perf test was always reporting success

--- test/performance/src/test/scala/io/ryuichi/whatsdone/BasicSimulation.scala
+++ test/performance/src/test/scala/io/ryuichi/whatsdone/BasicSimulation.scala
@@ -6,6 +6,7 @@ import io.gatling.http.Predef._
 class BasicSimulation extends Simulation {
 
   val COOKIE = "connect.sid=ENCODED_SESSION_ID"
+  val REQUEST_NAME = "Get Dones"
 
   val httpConf = http
     .baseURL("https://whatsdone-api.ryuichi.io")
@@ -16,10 +17,15 @@ class BasicSimulation extends Simulation {
     .acceptEncodingHeader("gzip, deflate")
 
   val scn = scenario("BasicSimulation")
-    .exec(http("Get Dones").get("/dones"))
+    .exec(http(REQUEST_NAME).get("/dones"))
     .pause(5)
 
-  setUp(scn.inject(atOnceUsers(100)))
-    .protocols(httpConf)
+  setUp(
+    scn.inject(atOnceUsers(100)).protocols(httpConf)
+  ).assertions(
+    details(REQUEST_NAME).responseTime.mean.lt(2000),
+    details(REQUEST_NAME).allRequests.percent.is(100),
+    details(REQUEST_NAME).failedRequests.percent.is(0)
+  )
 
 }
