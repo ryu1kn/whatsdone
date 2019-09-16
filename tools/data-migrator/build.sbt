eb7f0cd994a53d2ab0c71257e1a976900267780a
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-09-16 20:30:38 +1000

    Use Cats Effect to run the programme

--- tools/data-migrator/build.sbt
+++ tools/data-migrator/build.sbt
@@ -9,12 +9,12 @@ scalacOptions ++= Seq(
   "-unchecked",           // warn about unchecked type parameters
   "-feature",             // warn about misused language features
   "-language:higherKinds",// allow higher kinded types without `import scala.language.higherKinds`
-  "-Xlint",               // enable handy linter warnings
-  "-Xfatal-warnings"      // turn compiler warnings into errors
+  "-Xlint"                // enable handy linter warnings
 )
 
 libraryDependencies ++= Seq(
-  "org.typelevel" %% "cats-core" % "2.0.0-M4",
+  "org.typelevel" %% "cats-core" % "2.0.0",
+  "org.typelevel" %% "cats-effect" % "2.0.0",
   "org.scalatest" %% "scalatest" % "3.0.8" % "test",
   "software.amazon.awssdk" % "dynamodb" % "2.5.63"
 )
--- tools/data-migrator/src/main/scala/io/ryuichi/whatsdone/Main.scala
+++ tools/data-migrator/src/main/scala/io/ryuichi/whatsdone/Main.scala
@@ -2,35 +2,26 @@ package io.ryuichi.whatsdone
 
 import java.util.concurrent.CompletableFuture
 
-import cats.instances.string._
-import cats.syntax.semigroup._
+import cats.effect.{ExitCode, IO, IOApp}
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
 import software.amazon.awssdk.services.dynamodb.model.{AttributeValue, ScanRequest}
 
 import scala.jdk.CollectionConverters._
 
-object Main extends App {
-  println("Hello " |+| "Cats!")
-  DynamoTest.dumpItems(args(0))
+object Main extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] = {
+    for {
+      items <- DynamoTest.dumpItems(args(0))
+      _ <- IO(println(items))
+    } yield ExitCode.Success
+  }
 }
 
 object DynamoTest {
-  def dumpItems(tableName: String): Unit = {
+  def dumpItems(tableName: String): IO[List[Map[String, AttributeValue]]] = {
     val client = DynamoDbAsyncClient.create
     val response = client.scan(ScanRequest.builder.tableName(tableName).build())
     val items: CompletableFuture[List[Map[String, AttributeValue]]] = response.thenApply(_.items().asScala.toList.map(_.asScala.toMap))
-    items.whenComplete((items, err) => {
-      try {
-        if (items != null) {
-          items.foreach(println)
-        } else {
-          err.printStackTrace()
-        }
-      } finally {
-        client.close()
-      }
-    })
-
-    items.join()
+    IO(items.join())
   }
 }
