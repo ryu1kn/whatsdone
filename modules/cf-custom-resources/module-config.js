e97200c344edf0352c3bf7325f27953578507074
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-12-28 19:50:04 +1100

    Removed unused modules

--- modules/cf-custom-resources/module-config.js
+++
@@ -1,8 +0,0 @@
-
-const { deploymentOutputsBucket, artifactBucket } = require('../../config/common');
-
-module.exports = {
-  deploymentOutputsBucket,
-  artifactBucket,
-  artifactBasePath: 'cf-custom-resources'
-};
--- modules/cf-custom-resources/src/apigateway-deployment/index.js
+++
@@ -1,6 +0,0 @@
-
-const fetch = require('node-fetch');
-const ApiGatewayDeploymentHandlerFactory = require('./lib/handler');
-const apiGatewayDeploymentHandler = new ApiGatewayDeploymentHandlerFactory({fetch}).create();
-
-module.exports = (...args) => apiGatewayDeploymentHandler.handle(...args);
