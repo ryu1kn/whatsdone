ccd3649713ef8c45ee9cea76f798c2a1c6869bab
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-12-29 21:13:56 +1100

    [ci] Simplify the deployment pipeline
    Always deploy all modules. If there are no changes, CloudFormation won't redeploy

--- buildmate.config.js
+++ buildmate.config.js
@@ -1,23 +1,12 @@
 
 module.exports = {
   tasks: [
-    // Build modules
+    // What's Done
     {
-      description: 'Build whatsdone-assets',
-      path: /^(modules\/whatsdone-assets)\/.*/,
+      description: "Build & Deploy What's Done",
+      path: /^modules\/.*/,
       command: `
-        cd $BM_PATH_VAR_1 \\
-            && yarn install \\
-            && yarn run lint`
-    },
-    {
-      description: 'Build whatsdone-app',
-      path: /^(modules\/whatsdone-app)\/.*/,
-      command: `
-        cd $BM_PATH_VAR_1/src \\
-            && yarn install \\
-            && yarn run lint \\
-            && yarn test`
+        BUILD_NUMBER="$TRAVIS_BUILD_NUMBER" ./deploy-system.sh`
     },
 
     // Build tools
@@ -25,23 +14,6 @@ module.exports = {
       description: 'Build tools/copy-done-table',
       path: /^(tools\/copy-done-table)\/.*/,
       command: 'cd $BM_PATH_VAR_1 && make test'
-    },
-
-    // Deploy modules
-    {
-      description: 'Deploy module',
-      path: /^(modules\/[^/]+)\/.*/,
-      command: 'cd $BM_PATH_VAR_1 && yarn run deploy --env $ENV_NAME --region $AWS_REGION'
-    },
-    {
-      description: 'Deploy API',  // XXX: whatsdone-api gets deployed twice if both -app and -api are updated
-      path: /^(modules)\/whatsdone-app\/.*/,
-      command: 'cd $BM_PATH_VAR_1/whatsdone-api && yarn run deploy --env $ENV_NAME --region $AWS_REGION'
-    },
-    {
-      description: 'Deploy whatsdone-assets app',
-      path: /^(modules\/whatsdone-assets)\/.*/,
-      command: 'cd $BM_PATH_VAR_1 && yarn install && yarn run build && yarn run deploy:app'
     }
   ]
 };
