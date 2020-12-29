6a4e971f28102ece21524a24c93062ad9f55fea4
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-12-29 21:20:25 +1100

    [ci] Disabled scheduled build

--- travis-build.sh
+++ travis-build.sh
@@ -3,9 +3,7 @@
 set -euo pipefail
 
 function main() {
-    if [ "$TRAVIS_EVENT_TYPE" = "cron" ] ; then
-        ENV_NAME=ci deploy_system
-    elif [ "$TRAVIS_BRANCH" = "master" ] ; then
+    if [[ "$TRAVIS_BRANCH" = "master" ]] ; then
         ENV_NAME=prod build_and_deploy
     else
         ENV_NAME=dev-ryuichi build_and_deploy
@@ -13,17 +11,13 @@ function main() {
 }
 
 function build_and_deploy() {
-    echo Commit range: $TRAVIS_COMMIT_RANGE
+    echo "Commit range: $TRAVIS_COMMIT_RANGE"
 
     echo "List of changed file(s) in the range:"
-    git diff --name-only $TRAVIS_COMMIT_RANGE
+    git diff --name-only "$TRAVIS_COMMIT_RANGE"
     echo
 
-    git diff --name-only $TRAVIS_COMMIT_RANGE | BUILD_NUMBER=$TRAVIS_BUILD_NUMBER ./node_modules/.bin/buildmate
-}
-
-function deploy_system() {
-    BUILD_NUMBER=$TRAVIS_BUILD_NUMBER ./deploy-system.sh
+    git diff --name-only "$TRAVIS_COMMIT_RANGE" | BUILD_NUMBER="$TRAVIS_BUILD_NUMBER" ./node_modules/.bin/buildmate
 }
 
 main "$@"
