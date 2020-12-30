be928c01b47b3cf0a387a4addeceac4df14bde55
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-12-30 22:55:19 +1100

    [ci] Trigger deployment with harmless changes

--- modules/whatsdone-app/build.sh
+++ modules/whatsdone-app/build.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 build_dir=built
-dist_dir=$npm_package_config_BUILD_DIR_NAME
+dist_dir="$npm_package_config_BUILD_DIR_NAME"
 
-mkdir -p $dist_dir
-cp -r package.json $build_dir/index.js $build_dir/lib $dist_dir
-(cd $dist_dir && yarn install --production)
+mkdir -p "$dist_dir"
+cp -r package.json "$build_dir/index.js" "$build_dir/lib" "$dist_dir"
+(cd "$dist_dir" && yarn install --production)
