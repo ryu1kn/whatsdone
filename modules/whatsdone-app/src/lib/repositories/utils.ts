61dfa0eebf7a5d0fa841d9d1551b71110943b487
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-01-16 00:04:33 +1100

    [backend] Rewrote with object destructuring. Easier to read

--- modules/whatsdone-app/src/lib/repositories/utils.ts
+++ modules/whatsdone-app/src/lib/repositories/utils.ts
@@ -1,9 +1,8 @@
 
 export const MONTH_LENGTH = 'YYYY-MM'.length;
 
-export function getDoneWithMonth (done: {date?: string}) {
-  if (!done.date) return done;
-  return Object.assign({}, done, {
+export const getDoneWithMonth = (done: {date?: string}) =>
+  !done.date ? done : {
+    ...done,
     month: done.date.substr(0, MONTH_LENGTH)
-  });
-}
+  };
