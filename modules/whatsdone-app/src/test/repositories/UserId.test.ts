3ca1c23ced8656478f90c500d3fc1b6089f8a2d3
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-01-16 19:26:20 +1100

    [backend] Refactoring on UserIdRepository

--- modules/whatsdone-app/src/test/repositories/UserId.test.ts
+++ modules/whatsdone-app/src/test/repositories/UserId.test.ts
@@ -3,55 +3,42 @@ import UserIdRepository from '../../lib/repositories/UserId';
 import ServiceFactory from '../../lib/ServiceFactory';
 import * as td from 'testdouble';
 import {PromiseResult} from 'aws-sdk/lib/request';
-import {AWSError} from 'aws-sdk';
+import {AWSError, DynamoDB} from 'aws-sdk';
 import {GetItemOutput, QueryOutput} from 'aws-sdk/clients/dynamodb';
 import {deepStrictEqual} from 'assert';
 
 describe('Server UserIdRepository', () => {
+  const awsResponse = (res: any) => ({promise: () => Promise.resolve(res)});
+
+  const docClient = td.instance(DynamoDB.DocumentClient);
+  td.when(docClient.get({
+    TableName: 'USER_ID_TABLE_NAME',
+    Key: {id: 'USER_ID'}
+  })).thenReturn(awsResponse({
+    Item: {cognitoUserId: 'COGNITO_USER_ID'}
+  } as unknown as PromiseResult<GetItemOutput, AWSError>));
+  td.when(docClient.query({
+    TableName: 'USER_ID_TABLE_NAME',
+    IndexName: 'cognitoUserId',
+    KeyConditionExpression: 'cognitoUserId = :hkey',
+    ExpressionAttributeValues: {':hkey': 'COGNITO_USER_ID'},
+    ProjectionExpression: 'id'
+  })).thenReturn(awsResponse({
+    Items: [{id: 'OLD_USER_ID'}]
+  } as unknown as PromiseResult<QueryOutput, AWSError>));
+
+  ServiceLocator.load({createDynamoDBDocumentClient: () => docClient} as ServiceFactory);
+  const repository = new UserIdRepository('USER_ID_TABLE_NAME');
 
   it('finds a cognito user id', async () => {
-    const dynamoDBDocumentClient = createDynamoDBDocumentClient();
-    const repository = createUserIdRepository(dynamoDBDocumentClient);
     const result = await repository.getCognitoUserId('USER_ID');
+
     deepStrictEqual(result, 'COGNITO_USER_ID');
   });
 
   it('finds a user id', async () => {
-    const dynamoDBDocumentClient = createDynamoDBDocumentClient();
-    const repository = createUserIdRepository(dynamoDBDocumentClient);
     const result = await repository.getByCognitoUserId('COGNITO_USER_ID');
+
     deepStrictEqual(result, 'OLD_USER_ID');
   });
-
-  function createDynamoDBDocumentClient(): AWS.DynamoDB.DocumentClient {
-    const docClient = td.object('get') as AWS.DynamoDB.DocumentClient;
-    td.when(docClient.get({
-      TableName: 'USER_ID_TABLE_NAME',
-      Key: {id: 'USER_ID'}
-    })).thenReturn({
-      promise: () => Promise.resolve({
-        Item: {cognitoUserId: 'COGNITO_USER_ID'}
-      } as unknown as PromiseResult<GetItemOutput, AWSError>)
-    });
-    td.when(docClient.query({
-      TableName: 'USER_ID_TABLE_NAME',
-      IndexName: 'cognitoUserId',
-      KeyConditionExpression: 'cognitoUserId = :hkey',
-      ExpressionAttributeValues: {':hkey': 'COGNITO_USER_ID'},
-      ProjectionExpression: 'id'
-    })).thenReturn({
-      promise: () => Promise.resolve({
-        Items: [{id: 'OLD_USER_ID'}]
-      } as unknown as PromiseResult<QueryOutput, AWSError>)
-    });
-    return docClient;
-  }
-
-  function createUserIdRepository(dynamoDBDocumentClient: AWS.DynamoDB.DocumentClient) {
-    ServiceLocator.load({
-      createDynamoDBDocumentClient: () => dynamoDBDocumentClient
-    } as ServiceFactory);
-    return new UserIdRepository('USER_ID_TABLE_NAME');
-  }
-
 });
