0e0846fecbbd01b83b04e7859c415a01ffdb370b
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-01-16 22:49:16 +1100

    [backend] Removed one more use of sinon

--- modules/whatsdone-app/src/test/request-processors/DeleteDone.test.ts
+++ modules/whatsdone-app/src/test/request-processors/DeleteDone.test.ts
@@ -1,37 +1,31 @@
 import DeleteDoneRequestProcessor from '../../lib/request-processors/DeleteDone';
 import ServiceLocator from '../../lib/ServiceLocator';
-import {throwError} from '../helper/TestUtils';
 import ServiceFactory from '../../lib/ServiceFactory';
 import {request, session} from '../helper/NormalisedRequestData';
-import {deepStrictEqual} from 'assert';
-import sinon = require('sinon');
+import {deepStrictEqual, rejects} from 'assert';
+import * as td from 'testdouble';
+import DoneRepository from '../../lib/repositories/Done';
 
 describe('Server DeleteDoneRequestProcessor', () => {
+  const doneRepository = td.instance(DoneRepository);
+  td.when(doneRepository.remove('DONE_ID', 'USER_ID')).thenResolve();
+  td.when(doneRepository.remove('DONE_ID_TRIGGER_ERROR', 'USER_ID'))
+    .thenReject(new Error('UNEXPECTED_ERROR'));
+
+  ServiceLocator.load({createDoneRepository: () => doneRepository} as ServiceFactory);
+
+  const processor = new DeleteDoneRequestProcessor();
+
   it('deletes a done item', async () => {
-    const doneRepository = {remove: sinon.stub().returns(Promise.resolve())};
-    ServiceLocator.load({createDoneRepository: () => doneRepository} as ServiceFactory);
-    const processor = new DeleteDoneRequestProcessor();
-
     const response = await processor.process(request, session);
+
     deepStrictEqual(response, {statusCode: '200'});
-    deepStrictEqual(doneRepository.remove.args[0], ['DONE_ID', 'USER_ID']);
   });
 
-  it('propagates error', () => {
-    ServiceLocator.load({
-      createDoneRepository: () => ({
-        remove: () => Promise.reject(new Error('UNEXPECTED_ERROR'))
-      })
-    } as ServiceFactory);
-    const processor = new DeleteDoneRequestProcessor();
+  it('propagates error', async () => {
+    const problematicRequest = {...request, params: {id: 'DONE_ID_TRIGGER_ERROR'}};
 
-    return processor.process(request, session).then(
-      throwError,
-      e => {
-        deepStrictEqual(e.message, 'UNEXPECTED_ERROR');
-      }
-    );
+    await rejects(processor.process(problematicRequest, session), new Error('UNEXPECTED_ERROR'));
   });
-
 });
 
