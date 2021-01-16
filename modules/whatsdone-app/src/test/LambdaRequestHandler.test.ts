46649f44d3782c96a3e71259a23ce7f4aff96a5b
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-01-16 22:39:37 +1100

    [backend] Replace sinon with testdouble

--- modules/whatsdone-app/src/test/LambdaRequestHandler.test.ts
+++ modules/whatsdone-app/src/test/LambdaRequestHandler.test.ts
@@ -3,7 +3,13 @@ import ServiceLocator from '../lib/ServiceLocator';
 import {Event} from '../lib/models/Lambda';
 import ServiceFactory from '../lib/ServiceFactory';
 import {deepStrictEqual} from 'assert';
-import sinon = require('sinon');
+import * as td from 'testdouble';
+import LambdaRequestNormaliser, {Request} from '../lib/LambdaRequestNormaliser';
+import UserIdRepository from '../lib/repositories/UserId';
+import LambdaResponseFormatter from '../lib/LambdaResponseFormatter';
+import {Response} from '../lib/models/Request';
+import {RequestProcessor} from '../lib/RequestProcessor';
+import RequestProcessErrorProcessor from '../lib/RequestProcessErrorProcessor';
 
 describe('Server LambdaRequestHandler', () => {
   const lambdaEvent: Event = {
@@ -20,60 +26,63 @@ describe('Server LambdaRequestHandler', () => {
       }
     }
   };
+  const lambdaEventTriggerError: Event = {...lambdaEvent, path: '/TRIGGER_ERROR'};
+
+  // TODO: Use valid-type data to remove the cast
+  const normalisedRequest = {
+    REQUEST_DATA: '..',
+    userInfo: {
+      userId: 'COGNITO_USER_ID',
+      username: 'COGNITO_USER_NAME'
+    }
+  } as unknown as Request;
+  const normalisedRequestTriggerError = {...normalisedRequest, REQUEST_DATA: 'WILL_TRIGGER_ERROR'} as unknown as Request;
+
+  const error = new Error('UNEXPECTED_ERROR');
+
+  const requestNormaliser = td.instance(LambdaRequestNormaliser);
+  td.when(requestNormaliser.normalise(lambdaEvent)).thenReturn(normalisedRequest);
+  td.when(requestNormaliser.normalise(lambdaEventTriggerError))
+    .thenReturn(normalisedRequestTriggerError);
+
+  const userIdRepository = td.instance(UserIdRepository);
+  td.when(userIdRepository.getByCognitoUserId('COGNITO_USER_ID')).thenResolve('USER_ID');
+
+  const responseFormatter = td.instance(LambdaResponseFormatter);
+  td.when(responseFormatter.format('RESPONSE' as unknown as Response)).thenReturn('LAMBDA_RESPONSE' as any);
+  td.when(responseFormatter.format('ERROR_RESPONSE' as unknown as Response)).thenReturn('LAMBDA_RESPONSE_FOR_ERROR' as any);
+
+  const requestProcessor = td.object<RequestProcessor>();
+  td.when(requestProcessor.process(
+    normalisedRequest,
+    {userId: 'USER_ID', username: 'COGNITO_USER_NAME'}
+  )).thenResolve('RESPONSE' as any);
+  td.when(requestProcessor.process(
+    normalisedRequestTriggerError,
+    {userId: 'USER_ID', username: 'COGNITO_USER_NAME'}
+  )).thenReject(error);
+
+  const requestProcessErrorProcessor = td.instance(RequestProcessErrorProcessor);
+  td.when(requestProcessErrorProcessor.process(error)).thenReturn('ERROR_RESPONSE' as any);
+
+  ServiceLocator.load({
+    createLambdaRequestNormaliser: () => requestNormaliser,
+    createUserIdRepository: () => userIdRepository,
+    createLambdaResponseFormatter: () => responseFormatter,
+    createRequestProcessErrorProcessor: () => requestProcessErrorProcessor
+  } as ServiceFactory);
+
+  const handler = new LambdaRequestHandler(requestProcessor);
 
   it('bridges lambda request/response world to lambda agnostic http request processor', async () => {
-    const normalisedRequest = {
-      REQUEST_DATA: '..',
-      userInfo: {
-        userId: 'COGNITO_USER_ID',
-        username: 'COGNITO_USER_NAME'
-      }
-    };
-    const requestNormaliser = {normalise: sinon.stub().returns(normalisedRequest)};
-    const userIdRepository = {getByCognitoUserId: sinon.stub().returns(Promise.resolve('USER_ID'))};
-    const responseFormatter = {format: sinon.stub().returns('LAMBDA_RESPONSE')};
-    const requestProcessor = {process: sinon.stub().returns(Promise.resolve('RESPONSE'))};
-    ServiceLocator.load({
-      createLambdaRequestNormaliser: () => requestNormaliser,
-      createUserIdRepository: () => userIdRepository,
-      createLambdaResponseFormatter: () => responseFormatter,
-      createRequestProcessErrorProcessor: () => {}
-    } as ServiceFactory);
-    const handler = new LambdaRequestHandler(requestProcessor);
-
     const response = await handler.handle(lambdaEvent);
-    deepStrictEqual(requestNormaliser.normalise.args[0], [lambdaEvent]);
-    deepStrictEqual(userIdRepository.getByCognitoUserId.args[0], ['COGNITO_USER_ID']);
-    deepStrictEqual(requestProcessor.process.args[0], [
-      normalisedRequest,
-      {
-        userId: 'USER_ID',
-        username: 'COGNITO_USER_NAME'
-      }
-    ]);
-    deepStrictEqual(responseFormatter.format.args[0], ['RESPONSE']);
+
     deepStrictEqual(response, 'LAMBDA_RESPONSE');
   });
 
   it('catches an exception occurred during request process step', async () => {
-    const requestNormaliser = {normalise: () => ({
-      REQUEST_DATA: '..',
-      userInfo: {sub: 'COGNITO_USER_ID'}
-    })};
-    const responseFormatter = {format: sinon.stub().returns('LAMBDA_RESPONSE')};
-    const requestProcessor = {process: () => Promise.reject(new Error('UNEXPECTED_ERROR'))};
-    const requestProcessErrorProcessor = {process: sinon.stub().returns('ERROR_RESPONSE')};
-    ServiceLocator.load({
-      createLambdaRequestNormaliser: () => requestNormaliser,
-      createUserIdRepository: () => ({getByCognitoUserId: () => Promise.resolve()}),
-      createLambdaResponseFormatter: () => responseFormatter,
-      createRequestProcessErrorProcessor: () => requestProcessErrorProcessor
-    } as ServiceFactory);
-    const handler = new LambdaRequestHandler(requestProcessor);
+    const response = await handler.handle(lambdaEventTriggerError);
 
-    const response = await handler.handle(lambdaEvent);
-    deepStrictEqual(requestProcessErrorProcessor.process.args[0][0].message, 'UNEXPECTED_ERROR');
-    deepStrictEqual(responseFormatter.format.args[0], ['ERROR_RESPONSE']);
-    deepStrictEqual(response, 'LAMBDA_RESPONSE');
+    deepStrictEqual(response, 'LAMBDA_RESPONSE_FOR_ERROR');
   });
 });
