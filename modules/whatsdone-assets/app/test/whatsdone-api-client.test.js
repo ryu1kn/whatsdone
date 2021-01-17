4882052424efb2f4b2cbeb494615930b9528588a
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-01-17 15:28:00 +1100

    [frontend] Removed sinon from the tests

--- modules/whatsdone-assets/app/test/whatsdone-api-client.test.js
+++ modules/whatsdone-assets/app/test/whatsdone-api-client.test.js
@@ -1,98 +1,79 @@
 import 'babel-polyfill';
 import test from 'tape';
-import sinon from 'sinon';
+import * as td from 'testdouble';
 
 import ServiceLocator from '../src/service-locator';
 import WhatsdoneApiClient from '../src/whatsdone-api-client';
 import ServiceFactory from '../src/service-factory';
 
-test('WhatsdoneApiClient fetch all done items', async t => {
-  t.plan(1);
+const baseOption = {
+  mode: 'cors',
+  credentials: 'include',
+  headers: {Authorization: 'JWT_ID_TOKEN'}
+};
 
-  const smartFetch = fakeSmartFetch();
-  const apiClient = new WhatsdoneApiClient();
+const smartFetch = td.function();
+td.when(smartFetch('/appConfig.json')).thenResolve({body: {API_ORIGIN: 'https://api_origin'}});
+td.when(smartFetch('https://api_origin/dones', baseOption)).thenResolve('RESPONSE_1');
+td.when(smartFetch('https://api_origin/dones?nextKey=NEXT%20KEY', baseOption)).thenResolve('RESPONSE_2');
+td.when(smartFetch('https://api_origin/dones', Object.assign({}, baseOption, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    Authorization: 'JWT_ID_TOKEN'
+  },
+  body: 'KEY=VALUE'
+}))).thenResolve('RESPONSE_3');
+td.when(smartFetch('https://api_origin/dones/DONE_ID', Object.assign({}, baseOption, {method: 'DELETE'})))
+  .thenResolve(('RESPONSE_4'));
 
-  await apiClient.getDones()
-
-  t.deepEqual(smartFetch.args[2], [
-    'https://api_origin/dones',
-    {
-      mode: 'cors',
-      credentials: 'include',
-      headers: {
-        Authorization: 'JWT_ID_TOKEN'
-      }
-    }
-  ]);
-});
-
-test('WhatsdoneApiClient fetch done items by sending a key', async t => {
-  t.plan(1);
-
-  const smartFetch = fakeSmartFetch();
-  const apiClient = new WhatsdoneApiClient();
-
-  await apiClient.getDones('NEXT KEY')
-
-  t.deepEqual(smartFetch.args[2][0], 'https://api_origin/dones?nextKey=NEXT%20KEY');
-});
-
-test('WhatsdoneApiClient record new done item', async t => {
-  t.plan(1);
-
-  const smartFetch = fakeSmartFetch();
-  const apiClient = new WhatsdoneApiClient();
-
-  await apiClient.postDone({KEY: 'VALUE'})
-
-  t.deepEqual(smartFetch.args[2], [
-    'https://api_origin/dones',
-    {
-      method: 'POST',
-      mode: 'cors',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        Authorization: 'JWT_ID_TOKEN'
-      },
-      body: 'KEY=VALUE'
-    }
-  ]);
-});
-
-test('WhatsdoneApiClient deletes one done item', async t => {
-  t.plan(1);
-
-  const smartFetch = fakeSmartFetch();
-  const apiClient = new WhatsdoneApiClient();
-
-  await apiClient.deleteDone('DONE_ID')
-
-  t.deepEqual(smartFetch.args[2], [
-    'https://api_origin/dones/DONE_ID',
-    {
-      method: 'DELETE',
-      mode: 'cors',
-      credentials: 'include',
-      headers: {
-        Authorization: 'JWT_ID_TOKEN'
-      },
-    }
-  ]);
-});
-
-function fakeSmartFetch() {
-  const stub = sinon.stub();
-  const response = {
-    status: 200,
-    body: {API_ORIGIN: 'https://api_origin'}
-  };
+const createWhatsdoneApiClient = () => {
   ServiceLocator.load(new ServiceFactory(), {
-    createSmartFetch: () => stub.returns(Promise.resolve(response)),
+    createSmartFetch: () => smartFetch,
     createCognitoUserInitialiser: () => ({}),
     createCookieStorage: () => ({
       getItem: () => 'JWT_ID_TOKEN'
     })
   });
-  return stub;
-}
+  return new WhatsdoneApiClient();
+};
+
+test('WhatsdoneApiClient fetch all done items', async t => {
+  t.plan(1);
+
+  const apiClient = createWhatsdoneApiClient();
+
+  const response = await apiClient.getDones()
+
+  t.deepEqual(response, 'RESPONSE_1');
+});
+
+test('WhatsdoneApiClient fetch done items by sending a key', async t => {
+  t.plan(1);
+
+  const apiClient = createWhatsdoneApiClient();
+
+  const response = await apiClient.getDones('NEXT KEY')
+
+  t.deepEqual(response, 'RESPONSE_2');
+});
+
+test('WhatsdoneApiClient record new done item', async t => {
+  t.plan(1);
+
+  const apiClient = createWhatsdoneApiClient();
+
+  const response = await apiClient.postDone({KEY: 'VALUE'})
+
+  t.deepEqual(response, 'RESPONSE_3');
+});
+
+test('WhatsdoneApiClient deletes one done item', async t => {
+  t.plan(1);
+
+  const apiClient = createWhatsdoneApiClient();
+
+  const response = await apiClient.deleteDone('DONE_ID')
+
+  t.deepEqual(response, 'RESPONSE_4');
+});
