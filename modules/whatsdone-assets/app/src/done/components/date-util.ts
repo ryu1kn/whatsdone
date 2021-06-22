1fe47d593c1d270e57076614f8e28183ba374e2c
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-06-22 21:12:34 +1000

    [frontend] Moved get friendly day label logic

--- modules/whatsdone-assets/app/src/done/components/date-util.ts
+++ modules/whatsdone-assets/app/src/done/components/date-util.ts
@@ -3,6 +3,30 @@ const pad0 = (n: number) => n < 10 ? `0${n}` : String(n);
 export const getLocalDateString = (d: Date) =>
   `${d.getFullYear()}-${pad0(d.getMonth() + 1)}-${pad0(d.getDate())}`;
 
+const ONEDAY_MS = 24 * 60 * 60 * 1000;
+
+const getDayLabel = (date: Date) =>
+  date.toLocaleDateString('en-GB', {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })
+
+export const getFriendlyDayLabel = (dateString: string) => {
+  const now = new Date();
+  const today = getLocalDateString(now);
+  const yesterday = getLocalDateString(new Date(now.getTime() - ONEDAY_MS));
+  switch (dateString) {
+    case today:
+      return 'Today';
+    case yesterday:
+      return 'Yesterday';
+    default:
+      return getDayLabel(new Date(dateString));
+  }
+}
+
 const hourIn12 = (h: number) => h > 12 ? h - 12 : h;
 
 export const formatTime = (date: Date) => {
--- modules/whatsdone-assets/app/src/done/components/history.tsx
+++ modules/whatsdone-assets/app/src/done/components/history.tsx
@@ -4,34 +4,9 @@ import React from 'react';
 import DoneList from './done-list';
 import DoneLoader from './done-loader';
 import {DoneItem, DoneState} from '../reducer';
-import {getLocalDateString} from './date-util';
-
-const ONEDAY_MS = 24 * 60 * 60 * 1000;
+import {getFriendlyDayLabel, getLocalDateString} from './date-util';
 
 class DoneHistory extends React.Component<{done: DoneState}> {
-
-  getDayLabel(date: Date) {
-    return date.toLocaleDateString('en-GB', {
-      weekday: 'short',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    });
-  }
-
-  getFriendlyDayLabel(dateString: string) {
-    const now = new Date();
-    const today = getLocalDateString(now);
-    const yesterday = getLocalDateString(new Date(now.getTime() - ONEDAY_MS));
-    switch (dateString) {
-    case today:
-      return 'Today';
-    case yesterday:
-      return 'Yesterday';
-    default:
-      return this.getDayLabel(new Date(dateString));
-    }
-  }
 
   render() {
     const grouped = _.groupBy(
@@ -42,7 +17,7 @@ class DoneHistory extends React.Component<{done: DoneState}> {
       <div className="donehistory">
         {_.sortBy(_.toPairs(grouped), pair => pair[0]).reverse()
           .map((data, index) =>
-            <DoneList title={this.getFriendlyDayLabel(data[0])} data={data[1]} key={index} />
+            <DoneList title={getFriendlyDayLabel(data[0])} data={data[1]} key={index} />
           )}
         {this.props.done.nextKey ? <DoneLoader nextKey={this.props.done.nextKey} /> : ''}
       </div>
