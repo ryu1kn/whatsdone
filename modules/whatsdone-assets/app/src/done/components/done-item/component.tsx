1577b1fa9171897c9c3a1f2cd1fb4691b846ebf3
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-06-25 23:21:43 +1000

    [frontend] Convert to functional component

--- modules/whatsdone-assets/app/src/done/components/done-item/component.tsx
+++ modules/whatsdone-assets/app/src/done/components/done-item/component.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {getColorCode} from '../../../util';
 import {formatTime} from '../date-util';
 import {Converter} from 'showdown';
@@ -18,32 +17,30 @@ const getFirstLetter = (name: string) => (name || '').charAt(0).toUpperCase() ||
 
 const getIconColor = (name: string) => name ? getColorCode(name) : '#DDDDDD';
 
-export class DoneItem extends React.Component<DoneItemProps> {
+const createOnClickDelete = (deleteDone: () => void) => (e: any) => {
+  e.stopPropagation();
+  (e.target as Element).closest('.doneitem')?.setAttribute('style', 'display:none;');
+  deleteDone();
+};
 
-  private delete(e: any) {
-    e.stopPropagation();
-    (ReactDOM.findDOMNode(this) as Element).setAttribute('style', 'display:none;');
-    this.props.deleteDone(this.props.doneId);
-  }
-
-  render() {
-    const rawMarkup = converter.makeHtml(this.props.children.toString());
-    return (
-      <div className="doneitem">
-        <div className="doneitem__user">
-          <div className="doneitem__user-icon" style={{backgroundColor: getIconColor(this.props.username)}}>
-            {getFirstLetter(this.props.username)}
-          </div>
-          <div className="doneiten__user-name">{this.props.username}</div>
+export const DoneItem = (props: DoneItemProps) => {
+  const rawMarkup = converter.makeHtml(props.children.toString());
+  const deleteDone = () => props.deleteDone(props.doneId);
+  return (
+    <div className="doneitem">
+      <div className="doneitem__user">
+        <div className="doneitem__user-icon" style={{backgroundColor: getIconColor(props.username)}}>
+          {getFirstLetter(props.username)}
         </div>
-        <div>
-          <div className="doneitem__done-thing" dangerouslySetInnerHTML={{__html: rawMarkup}} />
-          <p className="doneitem__time">
-            {formatTime(this.props.date)}
-          </p>
-        </div>
-        <div className="doneitem__delete-action glyphicon glyphicon-trash" onClick={this.delete.bind(this)}></div>
+        <div className="doneiten__user-name">{props.username}</div>
       </div>
-    );
-  }
-}
+      <div>
+        <div className="doneitem__done-thing" dangerouslySetInnerHTML={{__html: rawMarkup}}/>
+        <p className="doneitem__time">
+          {formatTime(props.date)}
+        </p>
+      </div>
+      <div className="doneitem__delete-action glyphicon glyphicon-trash" onClick={createOnClickDelete(deleteDone)}/>
+    </div>
+  );
+};
