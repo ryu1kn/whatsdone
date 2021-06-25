87def6f85f0446ac7f8a3ad1cbe4662a91730eb8
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-06-25 22:30:11 +1000

    [frontend] Unit test for UI

---
+++ modules/whatsdone-assets/app/test/done/components/done-item.test.tsx
@@ -0,0 +1,20 @@
+/**
+ * @jest-environment jsdom
+ */
+import {DoneItem} from '../../../src/done/components/done-item/component';
+import {fireEvent, render, screen, waitFor} from '@testing-library/react';
+import React from 'react';
+import 'regenerator-runtime/runtime';
+
+test('Show easy to read time of the user region (This test passes in Melbourne)', async () => {
+  render(
+    <DoneItem doneId={'DONE_ID'} date={new Date('2021-06-25T12:06:00Z')}
+      username={'USERNAME'} key={0} deleteDone={() => {}}>
+      {'DONE SOMETHING'}
+    </DoneItem>
+  )
+
+  fireEvent.doubleClick(screen.getByText('DONE SOMETHING'))
+
+  await waitFor(() => screen.getByText('10:06 pm'))
+});
