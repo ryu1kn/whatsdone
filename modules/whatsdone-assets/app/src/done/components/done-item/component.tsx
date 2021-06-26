67b8f4882ced0e1f53d47bd4b3cb0ec890754c2d
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-06-26 12:24:34 +1000

    [frontend] Make DoneItem more self-explanatory

--- modules/whatsdone-assets/app/src/done/components/done-item/component.tsx
+++ modules/whatsdone-assets/app/src/done/components/done-item/component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {getColorCode} from '../../../util';
 import {formatTime} from '../date-util';
 import {Converter} from 'showdown';
+import {flow} from 'lodash';
 
 const converter = new Converter();
 
@@ -13,13 +14,19 @@ interface DoneItemProps {
   children: string
 }
 
+const DONE_CSS_NAMESPACE = 'doneitem';
+
 const getFirstLetter = (name: string) => (name || '').charAt(0).toUpperCase() || '?';
 
 const getIconColor = (name: string) => name ? getColorCode(name) : '#DDDDDD';
 
+const grabTopElement = (el: Element) => el.closest(`.${DONE_CSS_NAMESPACE}`);
+const hideElement = (el: Element) => el.setAttribute('style', 'display:none;');
+const hideDone = flow(grabTopElement, hideElement)
+
 const createOnClickDelete = (deleteDone: () => void) => (e: any) => {
   e.stopPropagation();
-  (e.target as Element).closest('.doneitem')?.setAttribute('style', 'display:none;');
+  hideDone(e.target as Element);
   deleteDone();
 };
 
@@ -27,12 +34,12 @@ export const DoneItem = (props: DoneItemProps) => {
   const rawMarkup = converter.makeHtml(props.children.toString());
   const deleteDone = () => props.deleteDone(props.doneId);
   return (
-    <div className="doneitem">
+    <div className={DONE_CSS_NAMESPACE}>
       <div className="doneitem__user">
         <div className="doneitem__user-icon" style={{backgroundColor: getIconColor(props.username)}}>
           {getFirstLetter(props.username)}
         </div>
-        <div className="doneiten__user-name">{props.username}</div>
+        <div className="doneitem__user-name">{props.username}</div>
       </div>
       <div>
         <div className="doneitem__done-thing" dangerouslySetInnerHTML={{__html: rawMarkup}}/>
