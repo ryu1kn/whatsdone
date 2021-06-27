bb8695688ebc0dd76af60e097093176cc43b9573
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-06-27 19:06:49 +1000

    [frontend] Rewrote tests with user interaction focus
    to make the tests less likely to break with refactoring

--- modules/whatsdone-assets/app/test/done/components/done-item.test.tsx
+++ modules/whatsdone-assets/app/test/done/components/done-item.test.tsx
@@ -1,18 +1,41 @@
 /**
  * @jest-environment jsdom
  */
-import {DoneItem} from '../../../src/done/components/done-item/component';
-import {render, screen, waitFor} from '@testing-library/react';
+import {render, waitFor} from '@testing-library/react';
 import React from 'react';
 import 'regenerator-runtime/runtime';
+import {Provider} from 'react-redux';
+import DonePage from '../../../src/done/components/page';
+import reducer from '../../../src/reducer';
+import {createStore} from 'redux';
+import WhatsdoneApiClient from '../../../src/whatsdone-api-client';
+import ServiceFactory from '../../../src/service-factory';
+import ServiceLocator from '../../../src/service-locator';
 
 test('Show easy to read time of the user region (This test passes in Melbourne)', async () => {
-  render(
-    <DoneItem doneId={'DONE_ID'} date={new Date('2021-06-25T12:06:00Z')}
-      username={'USERNAME'} key={0} deleteDone={() => {}} startEditDone={() => {}} updateDone={() => {}}>
-      {'DONE SOMETHING'}
-    </DoneItem>
+  ServiceLocator.load({
+    createWhatsdoneApiClient: () => ({getDones: (_nextKey?) => Promise.resolve({
+        status: 200,
+        body: {
+          items: [{
+            date: '2021-06-25T12:06:00Z',
+            doneThing: 'DONE SOMETHING',
+            id: 'DONE_ID',
+            userId: 'USER_ID',
+            username: 'USERNAME'
+          }]
+        }
+    })} as WhatsdoneApiClient)
+  } as ServiceFactory);
+
+  const store = createStore(reducer);
+  store.dispatch({type: 'API_READY'})
+
+  const { getByText } = render(
+    <Provider store={store}>
+      <DonePage/>
+    </Provider>
   )
 
-  await waitFor(() => screen.getByText('10:06 pm'))
+  await waitFor(() => getByText('10:06 pm'))
 });
