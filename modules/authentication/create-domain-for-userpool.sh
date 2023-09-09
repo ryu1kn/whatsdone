e869793fabcaeb6083aa8b8d26701b7586ae4f97
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2023-09-09 22:09:52 +1000

    Made trivial change to trigger a deployment

--- modules/authentication/create-domain-for-userpool.sh
+++ modules/authentication/create-domain-for-userpool.sh
@@ -8,8 +8,8 @@ readonly userPoolId="$(aws cognito-idp describe-user-pool-domain --domain "$USER
 if [[ "$userPoolId" = null ]] ; then
     aws cognito-idp create-user-pool-domain --user-pool-id "$USER_POOL_ID" --domain "$USER_POOL_DOMAIN" --region "$TASK_REGION"
 elif [[ "$userPoolId" = "$USER_POOL_ID" ]] ; then
-    echo "...User pool domain is already created for the specified user pool"
+    echo '...User pool domain is already created for the specified user pool'
 else
-    echo "...User pool domain is already taken by another user pool"
+    echo '...User pool domain is already taken by another user pool'
     exit 1
 fi
