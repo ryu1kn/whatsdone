4222f11e69412d628ae8f8240b647dac0215bbeb
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2023-09-10 16:23:31 +1000

    Use separate GitHub Actions jobs to surface different build steps on UI

--- buildmate.config.js
+++ buildmate.config.js
@@ -2,27 +2,12 @@
 module.exports = {
   tasks: [
     {
-      description: "Deploy to CI environment",
+      description: "Deploy to ENV_NAME environment",
       path: /^modules\/.*/,
-      command: 'ENV_NAME=ci ./deploy-system.sh'
-    },
-    {
-      description: "Run end-to-end tests on CI environment",
-      path: /^(modules|test\/e2e)\/.*/,
-      command: 'cd test/e2e && make test'
-    },
-    {
-      description: "Check if docker is available",
-      path: /.*/,
-      command: 'command -v docker && command -v docker-compose'
-    },
-    {
-      description: "Deploy to Production environment",
-      path: /^modules\/.*/,
-      command: 'ENV_NAME=prod ./deploy-system.sh'
+      command: './deploy-system.sh'
     },
 
-    // Build tools
+    // Build tools (to be migrated to GitHub Actions)
     {
       description: 'Build tools/copy-done-table',
       path: /^(tools\/copy-done-table)\/.*/,
